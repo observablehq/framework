1648afdf30e9a88fed23c60e30fa4d55d60e1cf5
Mike Bostock <mbostock@gmail.com>
2023-09-27 15:39:04 -0700

    eslint, prettier, etc.

--- src/preview.ts
+++ src/preview.ts
@@ -4,11 +4,19 @@ import {readFile} from "fs/promises";
 
 const md = MarkdownIt();
 const hostname = process.env.HOSTNAME ?? "127.0.0.1";
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ? +process.env.PORT : 3000;
+
+// TODO
+// - routing
+// - header and footer
+// - web socket
+// - file watching
+// - 'o' in the terminal opens the browser
 
 const server = createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
+  res.write(`<!DOCTYPE html>\n`);
   res.end(md.render(await readFile("./docs/index.md", "utf-8")));
 });
 
