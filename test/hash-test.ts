7287904c5634544a5ec54208fc8766471ec23264
Mike Bostock <mbostock@gmail.com>
2023-10-01 18:09:57 -0700

    describe tests

--- test/hash-test.ts
+++ test/hash-test.ts
@@ -1,7 +1,9 @@
 import {computeHash} from "../src/hash.js";
 import assert from "assert";
 
-it("computeHash(content) returns the expected result", () => {
-  assert.deepStrictEqual(computeHash(""), "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=");
-  assert.deepStrictEqual(computeHash("hello"), "LPJNul+wow4m6DsqxbninhsWHlwfp0JecwQzYpOLmCQ=");
+describe("computeHash(content)", () => {
+  it("returns the expected result", () => {
+    assert.deepStrictEqual(computeHash(""), "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=");
+    assert.deepStrictEqual(computeHash("hello"), "LPJNul+wow4m6DsqxbninhsWHlwfp0JecwQzYpOLmCQ=");
+  });
 });
