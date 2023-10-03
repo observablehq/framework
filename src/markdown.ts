9a924cf1e688ddd4882e3ac86d00cadc636c84b6
Mike Bostock <mbostock@gmail.com>
2023-10-02 19:39:49 -0700

    find placeholders in blocks

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,8 +1,8 @@
-import hljs from "highlight.js";
 import matter from "gray-matter";
+import hljs from "highlight.js";
 import MarkdownIt from "markdown-it";
-import {RuleInline} from "markdown-it/lib/parser_inline.js";
-import {RenderRule} from "markdown-it/lib/renderer.js";
+import type {RuleCore} from "markdown-it/lib/parser_core.js";
+import type {RenderRule} from "markdown-it/lib/renderer.js";
 import {computeHash} from "./hash.js";
 import {transpileJavaScript} from "./javascript.js";
 
@@ -18,18 +18,17 @@ interface ParseResult {
 }
 
 function makeFenceRenderer(baseRenderer: RenderRule): RenderRule {
-  return (tokens, idx, options, env, self) => {
-    const context = env as ParseContext;
-    let result = "";
+  return (tokens, idx, options, context: ParseContext, self) => {
     const token = tokens[idx];
     const [language, option] = token.info.split(" ");
+    let result = "";
     if (language === "js" && option !== "no-run") {
       const id = ++context.id;
       context.js += `\n${transpileJavaScript(token.content, id)}`;
       result += `<div id="cell-${id}" class="observablehq observablehq--block"></div>\n`;
     }
     if (language !== "js" || option === "show" || option === "no-run") {
-      result += baseRenderer(tokens, idx, options, env, self);
+      result += baseRenderer(tokens, idx, options, context, self);
     }
     return result;
   };
@@ -42,49 +41,103 @@ const CODE_BACKSLASH = 92;
 const CODE_QUOTE = 34;
 const CODE_SINGLE_QUOTE = 39;
 const CODE_BACKTICK = 96;
-const PLACEHOLDER_TYPE = "placeholder";
 
-const parsePlaceholder: RuleInline = (state, silent) => {
-  if (silent || state.pos + 2 > state.posMax) return false;
-  const marker1 = state.src.charCodeAt(state.pos);
-  const marker2 = state.src.charCodeAt(state.pos + 1);
-  if (!(marker1 === CODE_DOLLAR && marker2 === CODE_BRACEL)) return false;
-  let inQuote = 0;
-  let braces = 0;
-  for (let pos = state.pos + 2; pos < state.posMax; ++pos) {
-    const code = state.src.charCodeAt(pos);
-    if (code === CODE_BACKSLASH) {
-      ++pos; // skip next character
-      continue;
-    }
-    if (inQuote) {
-      if (code === inQuote) inQuote = 0;
-      continue;
-    }
-    switch (code) {
-      case CODE_QUOTE:
-      case CODE_SINGLE_QUOTE:
-      case CODE_BACKTICK:
-        inQuote = code;
-        break;
-      case CODE_BRACEL:
-        ++braces;
-        break;
-      case CODE_BRACER:
-        if (--braces < 0) {
-          const token = state.push(PLACEHOLDER_TYPE, "", 0);
-          token.content = state.src.slice(state.pos + 2, pos);
-          state.pos = pos + 1;
-          return true;
+function parsePlaceholder(content: string, replacer: (i: number, j: number) => void) {
+  let afterDollar = false;
+  for (let j = 0, n = content.length; j < n; ++j) {
+    const cj = content.charCodeAt(j);
+    if (cj === CODE_BACKSLASH) {
+      ++j; // skip next character
+    } else if (cj === CODE_DOLLAR) {
+      afterDollar = true;
+    } else {
+      if (cj === CODE_BRACEL && afterDollar) {
+        let inQuote = 0; // TODO detect comments, too
+        let braces = 0;
+        inner: for (let k = j + 1; k < n; ++k) {
+          const ck = content.charCodeAt(k);
+          if (inQuote) {
+            if (ck === inQuote) inQuote = 0;
+            continue;
+          }
+          switch (ck) {
+            case CODE_QUOTE:
+            case CODE_SINGLE_QUOTE:
+            case CODE_BACKTICK:
+              inQuote = ck;
+              break;
+            case CODE_BRACEL:
+              ++braces;
+              break;
+            case CODE_BRACER:
+              if (--braces < 0) {
+                replacer(j - 1, k + 1);
+                break inner;
+              }
+              break;
+          }
         }
+      }
+      afterDollar = false;
+    }
+  }
+}
+
+function transformPlaceholderBlock(token) {
+  const input = token.content;
+  const output: any[] = [];
+  let i = 0;
+  parsePlaceholder(input, (j, k) => {
+    output.push({...token, content: input.slice(i, j)});
+    output.push({type: "placeholder", content: input.slice(j + 2, k - 1)});
+    i = k;
+  });
+  if (i === 0) return token;
+  else if (i < input.length) output.push({...token, content: input.slice(i)});
+  return output;
+}
+
+function transformPlaceholderInline(token) {
+  const input = token.children;
+  const output: any[] = [];
+  for (const child of input) {
+    if (child.type === "text") {
+      let i = 0;
+      const content = child.content;
+      parsePlaceholder(content, (j, k) => {
+        output.push({...child, content: content.slice(i, j)});
+        output.push({type: "placeholder", content: content.slice(j + 2, k - 1)});
+        i = k;
+      });
+      if (i === 0) output.push(child);
+      else if (i < content.length) output.push({...child, content: content.slice(i)});
+    } else {
+      output.push(child);
+    }
+  }
+  return {...token, children: output};
+}
+
+const transformPlaceholder: RuleCore = (state) => {
+  const input = state.tokens;
+  const output: any[] = [];
+  for (const token of input) {
+    switch (token.type) {
+      case "html_block":
+        output.push(...transformPlaceholderBlock(token));
+        break;
+      case "inline":
+        output.push(transformPlaceholderInline(token));
+        break;
+      default:
+        output.push(token);
         break;
     }
   }
-  return false;
+  state.tokens = output;
 };
 
-const renderPlaceholder: RenderRule = (tokens, idx, _options, env) => {
-  const context = env as ParseContext;
+const renderPlaceholder: RenderRule = (tokens, idx, options, context: ParseContext) => {
   const id = ++context.id;
   const token = tokens[idx];
   context.js += `\n${transpileJavaScript(token.content, id, {inline: true})}`;
@@ -128,8 +181,8 @@ export function parseMarkdown(source: string): ParseResult {
       return ""; // defaults to escapeHtml(str)
     }
   });
-  md.inline.ruler.push(PLACEHOLDER_TYPE, parsePlaceholder);
-  md.renderer.rules[PLACEHOLDER_TYPE] = renderPlaceholder;
+  md.core.ruler.before("linkify", "placeholder", transformPlaceholder);
+  md.renderer.rules.placeholder = renderPlaceholder;
   md.renderer.rules.fence = makeFenceRenderer(md.renderer.rules.fence!);
   const context: ParseContext = {id: 0, js: ""};
   const tokens = md.parse(parts.content, context);
