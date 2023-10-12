a11328448d23000ccd0fd79d31765758ec83c818
Philippe Rivière <fil@rezo.net>
2023-10-12 13:43:09 +0200

    add help and version commands

--- bin/observable.ts
+++ bin/observable.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S node --loader tsx/esm
+import {readFileSync} from "node:fs";
 
 const command = process.argv.splice(2, 1)[0];
 
@@ -9,10 +10,23 @@ switch (command) {
   case "preview":
     import("../src/preview.js");
     break;
+  case "version":
+  case "-v":
+    console.log(version());
+    break;
+  case "help":
+  case "-h":
   default:
     console.error(`Usage: observable <command>`);
     console.error(`   build\tgenerate a static site`);
     console.error(`   preview\trun the live preview server`);
+    console.error(`   help (-h)\tshow this help`);
+    console.error(`   version (-v)\tshow version`);
     process.exit(1);
     break;
+}
+
+function version() {
+  const meta = JSON.parse(readFileSync("package.json", "utf-8"));
+  return `${meta.name} ${meta.version}`;
 }
