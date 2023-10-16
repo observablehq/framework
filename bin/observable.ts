d8c36a087a70864bd21a58ed2934006f774bf7ae
Wiltse Carpenter <wiltse@observablehq.com>
2023-10-16 11:37:58 -0700

    Add --version option to observable command

--- bin/observable.ts
+++ bin/observable.ts
@@ -3,6 +3,11 @@
 const command = process.argv.splice(2, 1)[0];
 
 switch (command) {
+  case "-v":
+  case "--version": {
+    import("../package.json").then(({version}: any) => console.log(version));
+    break;
+  }
   case "build":
     import("../src/build.js");
     break;
@@ -13,6 +18,7 @@ switch (command) {
     console.error(`Usage: observable <command>`);
     console.error(`   build\tgenerate a static site`);
     console.error(`   preview\trun the live preview server`);
+    console.error(` --version\tprint the version`);
     process.exit(1);
     break;
 }
