a5e5c33e997cc83d5a7478a31714f1fc886eb368
Philippe Rivière <fil@rezo.net>
2023-11-07 17:43:03 +0100

    apply suggestions from review

--- test/dataloaders-test.ts
+++ test/dataloaders-test.ts
@@ -5,20 +5,29 @@ import assert from "node:assert";
 describe("data loaders are called with the appropriate command", () => {
   it("a .js data loader is called with node", async () => {
     const outputPath = ".observablehq/data1.txt.tmp";
-    await runLoader("test/dataloaders/data1.js", outputPath);
-    assert.strictEqual(await readFile(outputPath, "utf-8"), "node\n");
-    await unlink(outputPath);
+    try {
+      await runLoader("test/dataloaders/data1.txt.js", outputPath);
+      assert.strictEqual(await readFile(outputPath, "utf-8"), "node\n");
+    } finally {
+      await unlink(outputPath);
+    }
   });
   it("a .ts data loader is called with tsx", async () => {
     const outputPath = ".observablehq/data2.txt.tmp";
-    await runLoader("test/dataloaders/data2.ts", outputPath);
-    assert.strictEqual(await readFile(outputPath, "utf-8"), "tsx\n");
-    await unlink(outputPath);
+    try {
+      await runLoader("test/dataloaders/data2.txt.ts", outputPath);
+      assert.strictEqual(await readFile(outputPath, "utf-8"), "tsx\n");
+    } finally {
+      await unlink(outputPath);
+    }
   });
   it("a .sh data loader is called as a shell script", async () => {
     const outputPath = ".observablehq/data3.txt.tmp";
-    await runLoader("test/dataloaders/data3.sh", outputPath);
-    assert.strictEqual(await readFile(outputPath, "utf-8"), "shell\n");
-    await unlink(outputPath);
+    try {
+      await runLoader("test/dataloaders/data3.txt.sh", outputPath);
+      assert.strictEqual(await readFile(outputPath, "utf-8"), "shell\n");
+    } finally {
+      await unlink(outputPath);
+    }
   });
 });
