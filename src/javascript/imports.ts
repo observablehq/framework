808a82609f0f7466489feae54448619cdc887163
Mike Bostock <mbostock@gmail.com>
2023-11-18 23:59:39 -0800

    fix #78; rewrite absolute paths

--- src/javascript/imports.ts
+++ src/javascript/imports.ts
@@ -1,5 +1,5 @@
 import {readFileSync} from "node:fs";
-import {dirname, join, normalize} from "node:path";
+import {dirname, join, normalize, relative} from "node:path";
 import {Parser} from "acorn";
 import type {ExportAllDeclaration, ExportNamedDeclaration, ImportDeclaration, ImportExpression, Node} from "acorn";
 import {simple} from "acorn-walk";
@@ -47,6 +47,7 @@ export function findImports(body: Node, root: string, sourcePath: string) {
   // find transitive imports. The path is always relative to the source path of
   // the Markdown file, even across transitive imports.
   function findLocalImports(path) {
+    if (path.startsWith("/")) path = relative(dirname(sourcePath), path);
     if (paths.has(path)) return;
     paths.add(path);
     imports.push({type: "local", name: path});
@@ -66,7 +67,7 @@ export function findImports(body: Node, root: string, sourcePath: string) {
       if (isStringLiteral(node.source)) {
         const value = getStringLiteralValue(node.source);
         if (isLocalImport(value, sourcePath)) {
-          findLocalImports(join(dirname(path), value));
+          findLocalImports(value.startsWith("/") ? normalize(value) : join(dirname(path), value));
         } else {
           imports.push({name: value, type: "global"});
           // non-local imports don't need to be traversed
@@ -92,7 +93,11 @@ export function resolveSources(input: string) {
   function resolveSource(node: ImportDeclaration | ImportExpression | ExportAllDeclaration | ExportNamedDeclaration) {
     if (isStringLiteral(node.source)) {
       const value = getStringLiteralValue(node.source);
-      output.replaceLeft(node.source.start, node.source.end, JSON.stringify(resolveImport(value)));
+      output.replaceLeft(
+        node.source.start,
+        node.source.end,
+        JSON.stringify(value.startsWith("/") ? `/_import${value}` : resolveImport(value))
+      );
     }
   }
 
@@ -133,7 +138,7 @@ export function rewriteImports(output: any, rootNode: JavaScriptNode, sourcePath
 }
 
 function relativeImport(sourcePath, value) {
-  return relativeUrl(sourcePath, join("/_import/", dirname(sourcePath), value));
+  return relativeUrl(sourcePath, join("/_import/", value.startsWith("/") ? "." : dirname(sourcePath), value));
 }
 
 function rewriteImportSpecifier(node) {
---
+++ test/output/build/imports/_import/top.js
@@ -0,0 +1,1 @@
+export const top = "top";
