358e186c41f7d2da17a96e5bd3358f69178b7e54
Michael Cooper <mythmon@gmail.com>
2023-11-20 14:37:51 -0800

    Add "observable login" command (#39)
    * Add "observable login" command
    
    * Use .observablehqrc on all platforms, with parent dir traversal
    
    * Use an environment variable for the ObservableHQ host
    
    * Add no-tty flow
    
    * handle cors
    
    * explicitly request scopes
    
    * update settings page url
    
    * Update required scopes
    
    * change config to .observablehq
    
    * Add auth whoami
    
    * fix login usage
    
    * write config in a pretty format
    
    * self review
    
    * lint
    
    * flatten command structure
    
    * Remove `obs-auth` script
    
    * Update bin/observable.ts
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>
    
    * user agent
    
    * a test for login
    
    * a test for whoami
    
    * forgot to include the package
    
    * fix tests and lint
    
    * Update src/auth.ts
    
    Co-authored-by: Philippe Rivière <fil@rezo.net>
    
    * switch from msw to undici for test mocking
    
    * one configurable handleGetUser mock endpoint, instead of two
    
    * don't try to mkdir for settings
    
    ---------
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>
    Co-authored-by: Philippe Rivière <fil@rezo.net>

---
+++ src/toolConfig.ts
@@ -0,0 +1,52 @@
+import fs from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+
+const configName = ".observablehq";
+
+interface Config {
+  auth?: {
+    id: string;
+    key: string;
+  };
+}
+
+export async function setObservableApiKey(id: string, key: string): Promise<void> {
+  const {config, configPath} = await loadConfig();
+  config.auth = {id, key};
+  await writeConfig({configPath, config});
+}
+
+export async function getObservableApiKey(): Promise<string | null> {
+  const {config} = await loadConfig();
+  return config.auth?.key ?? null;
+}
+
+async function loadConfig(): Promise<{configPath: string; config: Config}> {
+  let cursor = path.resolve(process.cwd());
+  while (true) {
+    const configPath = path.join(cursor, configName);
+    let content: string | null = null;
+    try {
+      content = await fs.readFile(configPath, "utf8");
+    } catch (err) {
+      const nextCursor = path.dirname(cursor);
+      if (nextCursor === cursor) break;
+      cursor = nextCursor;
+    }
+
+    if (content !== null) {
+      try {
+        return {config: JSON.parse(content), configPath};
+      } catch (err) {
+        console.error(`Problem parsing config file at ${configPath}: ${err}`);
+      }
+    }
+  }
+
+  return {config: {}, configPath: path.join(os.homedir(), configName)};
+}
+
+async function writeConfig({configPath, config}: {configPath: string; config: Config}): Promise<void> {
+  await fs.writeFile(configPath, JSON.stringify(config, null, 2));
+}
