6ecd8c96e37d1ab3b8d726e6b848503ada21c1c3
Mike Bostock <mbostock@gmail.com>
2023-11-27 19:33:25 -0800

    fix media query

--- src/client/sidebar.js
+++ src/client/sidebar.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector("#observablehq-sidebar-toggle");
 if (toggle) {
   let indeterminate = toggle.indeterminate;
   toggle.onclick = () => {
-    const matches = matchMedia("(min-width: calc(640px + 4rem + 0.5rem + 240px + 2rem))").matches;
+    const matches = matchMedia("(min-width: calc(640px + 17px * 4 + 240px + 17px * 2))").matches;
     if (indeterminate) (toggle.checked = !matches), (indeterminate = false);
     else if (toggle.checked === matches) indeterminate = true;
     toggle.indeterminate = indeterminate;
