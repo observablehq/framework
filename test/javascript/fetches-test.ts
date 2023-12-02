437632d21e1ecb6cb76229d858a72b3ab53078be
Mike Bostock <mbostock@gmail.com>
2023-12-01 20:53:11 -0800

    more tests

--- test/javascript/fetches-test.ts
+++ test/javascript/fetches-test.ts
@@ -45,6 +45,14 @@ describe("rewriteIfLocalFetch(node, output, references, sourcePath, options)", (
     assert.strictEqual(testFetch("./test.txt", "sub/test.js", {meta: true}), 'fetch(new URL("../../_file/sub/test.txt", import.meta.url))'); // prettier-ignore
     assert.strictEqual(testFetch("../test.txt", "sub/test.js", {meta: true}), 'fetch(new URL("../../_file/test.txt", import.meta.url))'); // prettier-ignore
   });
+  it("ignores fetches that don’t start with ./, ../, or /", () => {
+    assert.strictEqual(testFetch("test.txt", "test.js"), 'fetch("test.txt")');
+    assert.strictEqual(testFetch("sub/test.txt", "test.js"), 'fetch("sub/test.txt")');
+    assert.strictEqual(testFetch("test.txt", "sub/test.js"), 'fetch("test.txt")');
+    assert.strictEqual(testFetch("test.txt", "test.js", {meta: true}), 'fetch("test.txt")');
+    assert.strictEqual(testFetch("sub/test.txt", "test.js", {meta: true}), 'fetch("sub/test.txt")');
+    assert.strictEqual(testFetch("test.txt", "sub/test.js", {meta: true}), 'fetch("test.txt")');
+  });
   it("rewrites absolute fetches without meta", () => {
     assert.strictEqual(testFetch("/test.txt", "test.js"), 'fetch("./_file/test.txt")');
     assert.strictEqual(testFetch("/sub/test.txt", "test.js"), 'fetch("./_file/sub/test.txt")');
@@ -71,7 +79,54 @@ describe("rewriteIfLocalFetch(node, output, references, sourcePath, options)", (
     assert.strictEqual(input.slice(call.start, call.end), 'fetch("./test.txt")');
     const output = new Sourcemap(input);
     rewriteIfLocalFetch(call, output, [call.callee as Identifier], "test.js");
-    assert.strictEqual(String(output), '((fetch) => fetch("./test.txt"))(eval)');
+    assert.strictEqual(String(output), input);
+  });
+  it("ignores non-identifier calls", () => {
+    const input = 'window.fetch("./test.txt")';
+    const node = Parser.parseExpressionAt(input, 0, {ecmaVersion: 13}) as CallExpression;
+    const output = new Sourcemap(input);
+    rewriteIfLocalFetch(node, output, [], "test.js");
+    assert.strictEqual(String(output), input);
+  });
+  it("ignores non-fetch calls", () => {
+    const input = 'fletch("./test.txt")';
+    const node = Parser.parseExpressionAt(input, 0, {ecmaVersion: 13}) as CallExpression;
+    const output = new Sourcemap(input);
+    rewriteIfLocalFetch(node, output, [], "test.js");
+    assert.strictEqual(String(output), input);
+  });
+  it("rewrites single-quoted literals", () => {
+    const input = "fetch('./test.txt')";
+    const node = Parser.parseExpressionAt(input, 0, {ecmaVersion: 13}) as CallExpression;
+    const output = new Sourcemap(input);
+    rewriteIfLocalFetch(node, output, [], "test.js");
+    assert.strictEqual(String(output), 'fetch("./_file/test.txt")');
+  });
+  it("rewrites template-quoted literals", () => {
+    const input = "fetch(`./test.txt`)";
+    const node = Parser.parseExpressionAt(input, 0, {ecmaVersion: 13}) as CallExpression;
+    const output = new Sourcemap(input);
+    rewriteIfLocalFetch(node, output, [], "test.js");
+    assert.strictEqual(String(output), 'fetch("./_file/test.txt")');
+  });
+  it("ignores non-literal calls", () => {
+    const input = "fetch(`./${'test'}.txt`)";
+    const node = Parser.parseExpressionAt(input, 0, {ecmaVersion: 13}) as CallExpression;
+    const output = new Sourcemap(input);
+    rewriteIfLocalFetch(node, output, [], "test.js");
+    assert.strictEqual(String(output), input);
+  });
+  it("ignores URL fetches", () => {
+    assert.strictEqual(testFetch("https://example.com", "test.js"), 'fetch("https://example.com")');
+    assert.strictEqual(testFetch("https://example.com", "sub/test.js"), 'fetch("https://example.com")');
+    assert.strictEqual(testFetch("https://example.com", "test.js", {meta: true}), 'fetch("https://example.com")');
+    assert.strictEqual(testFetch("https://example.com", "sub/test.js", {meta: true}), 'fetch("https://example.com")');
+  });
+  it("ignores non-local path fetches", () => {
+    assert.strictEqual(testFetch("../test.txt", "test.js"), 'fetch("../test.txt")');
+    assert.strictEqual(testFetch("./../test.txt", "test.js"), 'fetch("./../test.txt")');
+    assert.strictEqual(testFetch("../../test.txt", "sub/test.js"), 'fetch("../../test.txt")');
+    assert.strictEqual(testFetch("./../../test.txt", "sub/test.js"), 'fetch("./../../test.txt")');
   });
 });
 
