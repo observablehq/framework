3467f705d39f4dbc56a5774cde3980dff8e6a205
Mike Bostock <mbostock@gmail.com>
2023-12-02 11:45:40 -0800

    width(target: Element)

--- src/client/stdlib/width.ts
+++ src/client/stdlib/width.ts
@@ -2,14 +2,14 @@ import {observe} from "./generators/observe.js";
 
 // Override the width definition to use main instead of body (and also use a
 // ResizeObserver instead of listening for window resize events).
-export function width() {
+export function width(target = document.querySelector<HTMLElement>("main")!) {
   return observe((notify: (width: number) => void) => {
     let width: number;
     const observer = new ResizeObserver(([entry]) => {
       const w = entry.contentRect.width;
       if (w !== width) notify((width = w));
     });
-    observer.observe(document.querySelector<HTMLElement>("main")!);
+    observer.observe(target);
     return () => observer.disconnect();
   });
 }
