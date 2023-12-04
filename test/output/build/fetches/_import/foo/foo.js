2cfac7e1b15e78b88938c3c7e1c403a1de1a000d
Mike Bostock <mbostock@gmail.com>
2023-12-03 16:38:52 -0800

    resolve local fetches with import.meta (#302)
    * demonstrate fetch resolution bug
    
    * resolve fetches with import.meta
    
    * regenerate tests
    
    * fix fetches test
    
    * import.meta.url for compatibility
    
    * test rewriteIfLocalFetch
    
    * more tests
    
    * meta → resolveMeta

--- test/output/build/fetches/_import/foo/foo.js
+++ test/output/build/fetches/_import/foo/foo.js
@@ -1,2 +1,2 @@
-export const fooJsonData = await fetch("../_file/foo/foo-data.json").then(d => d.json());
-export const fooCsvData = await fetch("../_file/foo/foo-data.csv").then(d => d.csv({ typed: true }));
+export const fooJsonData = await fetch(new URL("../../_file/foo/foo-data.json", import.meta.url)).then(d => d.json());
+export const fooCsvData = await fetch(new URL("../../_file/foo/foo-data.csv", import.meta.url)).then(d => d.text());
--- test/output/build/fetches/_import/top.js
+++ test/output/build/fetches/_import/top.js
@@ -1,3 +1,3 @@
-export {fooCsvData, fooJsonData} from "./foo/foo.js?sha=7a93b271ec78dd07db6d9265e7b82eacc1a1bb6682cd665f0f86ea2c0fbc7350";
-export const topJsonData = await fetch("./_file/top-data.json").then(d => d.json());
-export const topCsvData = await fetch("./_file/top-data.csv").then(d => d.csv({ typed: true }));
+export {fooCsvData, fooJsonData} from "./foo/foo.js?sha=ed706415f035efdd17afdfeeb09a5ff51231b2a43553a47ae89a6bf03039b1ee";
+export const topJsonData = await fetch(new URL("../_file/top-data.json", import.meta.url)).then(d => d.json());
+export const topCsvData = await fetch(new URL("../_file/top-data.csv", import.meta.url)).then(d => d.text());
