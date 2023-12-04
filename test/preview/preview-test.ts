111b6c5b7af91ac195fb6d40ac78353d154e46eb
Cindy <37343722+cinxmo@users.noreply.github.com>
2023-12-04 18:54:48 -0500

    additional preview tests

--- test/preview/preview-test.ts
+++ test/preview/preview-test.ts
@@ -1,77 +1,153 @@
+import {readFile, writeFile} from "node:fs/promises";
+import {setTimeout} from "timers/promises";
 import chai, {assert, expect} from "chai";
 import chaiHttp from "chai-http";
-import {preview} from "../../src/preview.js";
+import {WebSocket} from "ws";
+import type {ParseResult} from "../../src/markdown.js";
+import {parseMarkdown} from "../../src/markdown.js";
+import {preview as previewServer} from "../../src/preview.js";
 import type {PreviewOptions, PreviewServer} from "../../src/preview.js";
-import {mockJsDelivr} from "../mocks/jsdelivr.js";
 
-const testHostRoot = "test/preview/dashboard";
+const testRoot = "test/preview/dashboard";
 const testHostName = process.env.TEST_HOSTNAME ?? "127.0.0.1";
 const testPort = +(process.env.TEST_PORT ?? 8080);
 
 const testServerOptions: PreviewOptions = {
-  root: testHostRoot,
+  root: testRoot,
   hostname: testHostName,
   port: testPort,
   verbose: false
 };
 
-const testServerUrl = `http://${testHostName}:${testPort}`;
 chai.use(chaiHttp);
+
+const testServerUrl = `http://${testHostName}:${testPort}`;
 
 describe("preview server", () => {
   let testServer: PreviewServer["_server"];
 
-  mockJsDelivr();
-
   before(async () => {
-    testServer = (await preview(testServerOptions)).server;
+    testServer = (await previewServer(testServerOptions)).server;
   });
 
-  after(async () => {
-    if (testServer) testServer.close();
+  after(() => {
+    testServer?.close();
   });
 
-  it("should start a server", async () => {
-    const res = await chai.request(testServerUrl).get("/");
-    expect(res).to.have.status(200);
-    assert.ok(res.text);
+  describe("http", () => {
+    it("should start a server", async () => {
+      const res = await chai.request(testServerUrl).get("/");
+      expect(res).to.have.status(200);
+      assert.ok(res.text);
+    });
+
+    it("redirects /index to /", async () => {
+      const res = await chai.request(testServerUrl).get("/index").redirects(0);
+      expect(res).to.redirectTo(/^\/$/);
+      expect(res).to.have.status(302);
+    });
+
+    it("serves nested pages", async () => {
+      const res = await chai.request(testServerUrl).get("/code/code");
+      expect(res).to.have.status(200);
+      expect(res.text).to.have.string("This text is not visible by default.");
+    });
+
+    it("serves Observable runtime.js", async () => {
+      const res = await chai.request(testServerUrl).get("/_observablehq/runtime.js");
+      expect(res).to.have.status(200);
+      expect(res.body.toString()).to.have.string("@observablehq/runtime v5.9.5 Copyright 2023 Observable, Inc.");
+    });
+
+    it("serves Observable stdlib.js", async () => {
+      const res = await chai.request(testServerUrl).get("/_observablehq/stdlib.js");
+      expect(res).to.have.status(200);
+    });
+
+    it("serves dependencies from the public directory", async () => {
+      const res = await chai.request(testServerUrl).get("/_observablehq/style.css");
+      expect(res).to.have.status(200);
+      expect(res.text).to.have.string("font-family");
+    });
+
+    it("serves local imports", async () => {
+      const res = await chai.request(testServerUrl).get("/_import/format.js");
+      expect(res).to.have.status(200);
+      expect(res.text).to.have.string("function formatTitle(title)");
+    });
+
+    it("handles missing imports", async () => {
+      const res = await chai.request(testServerUrl).get("/_import/idontexist.js");
+      expect(res).to.have.status(404);
+      expect(res.text).to.have.string("404 page");
+    });
+
+    it("serves local files", async () => {
+      const res = await chai.request(testServerUrl).get("/_file/file.csv");
+      expect(res).to.have.status(200);
+      assert.ok(res.text);
+    });
+
+    it("handles missing files", async () => {
+      const res = await chai.request(testServerUrl).get("/_file/idontexist.csv");
+      expect(res).to.have.status(404);
+      expect(res.text).to.have.string("404 page");
+    });
   });
 
-  it.skip("redirects /index to /", async () => {
-    const res = await chai.request(testServerUrl).get("/index").redirects(0);
-    expect(res).to.redirectTo(/^\/$/);
-    expect(res).to.have.status(302);
-  });
+  describe("websocket", () => {
+    let testWebSocket: WebSocket;
+    let fileContent: string;
+    let pageContent: string;
+    let parsedContent: ParseResult;
+    let messages: string[] = [];
 
-  it("serves nested pages", async () => {
-    const res = await chai.request(testServerUrl).get("/code/code");
-    expect(res).to.have.status(200);
-    expect(res.text).to.have.string("This text is not visible by default.");
-  });
+    before(async () => {
+      pageContent = await readFile(`${testRoot}/index.md`, "utf-8");
+      fileContent = await readFile(`${testRoot}/file.csv`, "utf-8");
+      parsedContent = await parseMarkdown(pageContent, "test/preview/dashboard", "index.md");
+      testWebSocket = new WebSocket(Object.assign(new URL("/_observablehq", testServerUrl), {protocol: "ws"}));
 
-  // TODO - tests for /_observablehq and data loader requests
+      testWebSocket.onopen = () => {
+        testWebSocket.send(JSON.stringify({type: "hello", path: "/", hash: parsedContent.hash}));
+      };
 
-  it("serves local imports", async () => {
-    const res = await chai.request(testServerUrl).get("/_import/format.js");
-    expect(res).to.have.status(200);
-    expect(res.text).to.have.string("function formatTitle(title)");
-  });
+      testWebSocket.on("message", function message(data) {
+        messages.push(JSON.parse(data.toString()));
+      });
 
-  it("handles missing imports", async () => {
-    const res = await chai.request(testServerUrl).get("/_import/idontexist.js");
-    expect(res).to.have.status(404);
-    expect(res.text).to.have.string("404 page");
-  });
+      testWebSocket.onerror = (error) => {
+        console.error("websocket error", error);
+      };
 
-  it("serves local files", async () => {
-    const res = await chai.request(testServerUrl).get("/_file/file.csv");
-    expect(res).to.have.status(200);
-    assert.ok(res.text);
-  });
+      testWebSocket.onclose = () => {
+        console.info("websocket close");
+      };
+    });
 
-  it("handles missing files", async () => {
-    const res = await chai.request(testServerUrl).get("/_file/idontexist.csv");
-    expect(res).to.have.status(404);
-    expect(res.text).to.have.string("404 page");
+    after(async () => {
+      testWebSocket?.close();
+      testServer?.close();
+      // reset file contents
+      await writeFile(`${testRoot}/index.md`, pageContent);
+      await writeFile(`${testRoot}/file.csv`, fileContent);
+    });
+
+    it("watch .md file", async () => {
+      messages = [];
+      await setTimeout(500);
+      await writeFile(`${testRoot}/index.md`, pageContent + "\n\n<div>Hello</div>");
+      await setTimeout(1000);
+      expect(messages).to.have.length(1);
+      expect(messages[0]["type"]).to.equal("update");
+    });
+
+    it("watch file attachment", async () => {
+      messages = [];
+      writeFile(`${testRoot}/file.csv`, fileContent + "\n1880-02-01,-0.21");
+      await setTimeout(1000);
+      expect(messages).to.have.length(1);
+      expect(messages[0]["type"]).to.equal("refresh");
+    });
   });
 });
