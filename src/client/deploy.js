aa62a96caf4a584d6fc3aa21834650cb7e2fe347
Michael Cooper <mythmon@gmail.com>
2023-12-19 16:36:45 -0800

    fix deploy.js from bad merge

--- src/client/deploy.js
+++ src/client/deploy.js
@@ -4,10 +4,12 @@ const origin = process.env.OBSERVABLEHQ_ORIGIN;
 const parent = window.parent; // capture to prevent reassignment
 
 function messaged(event) {
-  if (!event.isTrusted || event.origin !== origin || event.source !== parent) return;
+  if (!event.isTrusted || event.origin !== origin || event.source !== parent || !event.data.observablehq) return;
   event.stopImmediatePropagation();
   const message = event.data;
-  if (message.type === "load_script") {
+  if (message.type === "hello") {
+    postMessage({type: "hello"});
+  } else if (message.type === "load_script") {
     import(message.url).then(
       () => postMessage({type: "load_script_complete", url: message.url}),
       (error) => postMessage({type: "load_script_error", url: message.url, error: error.message})
@@ -20,5 +22,5 @@ addEventListener("message", messaged);
 postMessage({type: "hello"});
 
 function postMessage(message) {
-  parent.postMessage(message, origin);
+  parent.postMessage({observablehq: true, ...message}, origin);
 }
