7e50c19c2dbf802b6fc8334f3396d51d11f896ed
Michael Cooper <mythmon@gmail.com>
2023-12-20 09:24:06 -0800

    add message id and remove observablehq check

--- src/client/deploy.js
+++ src/client/deploy.js
@@ -4,23 +4,24 @@ const origin = process.env.OBSERVABLEHQ_ORIGIN;
 const parent = window.parent; // capture to prevent reassignment
 
 function messaged(event) {
-  if (!event.isTrusted || event.origin !== origin || event.source !== parent || !event.data.observablehq) return;
+  if (!event.isTrusted || event.origin !== origin || event.source !== parent) return;
   event.stopImmediatePropagation();
   const message = event.data;
   if (message.type === "hello") {
     postMessage({type: "hello"});
   } else if (message.type === "load_script") {
     import(message.url).then(
-      () => postMessage({type: "load_script_complete", url: message.url}),
-      (error) => postMessage({type: "load_script_error", url: message.url, error: error.message})
+      () => postMessage({type: "load_script_complete", url: message.url, re: message.id}),
+      (error) => postMessage({type: "load_script_error", url: message.url, error: error.message, re: message.id})
     );
   }
 }
 
-addEventListener("message", messaged);
-
-postMessage({type: "hello"});
-
+let fingerprint = `c-${Math.random().toString(36).slice(2, 8).padStart(6, "0")}`;
+let nextId = 0;
 function postMessage(message) {
-  parent.postMessage({observablehq: true, ...message}, origin);
+  parent.postMessage({id: `${fingerprint}-${++nextId}`, ...message}, origin);
 }
+
+addEventListener("message", messaged);
+postMessage({type: "hello"});
