4b7d003e2fb409b57ec97d712f80301defe554ad
Visnu Pitiyanuvath <visnup@gmail.com>
2023-12-19 19:24:28 -0800

    Don't fail tests

--- test/config-test.ts
+++ test/config-test.ts
@@ -1,7 +1,9 @@
 import assert from "node:assert";
 import {normalizeConfig as config, mergeToc, readConfig} from "../src/config.js";
+import {Telemetry} from "../src/telemetry.js";
 
 const root = "test/input/build/config";
+const telemetry = new Telemetry();
 
 describe("readConfig(undefined, root)", () => {
   it("imports the config file at the specified root", async () => {
@@ -22,7 +24,8 @@ describe("readConfig(undefined, root)", () => {
       deploy: {
         workspace: "acme",
         project: "bi"
-      }
+      },
+      telemetry
     });
   });
   it("returns the default config if no config file is found", async () => {
@@ -35,7 +38,8 @@ describe("readConfig(undefined, root)", () => {
       toc: {label: "Contents", show: true},
       pager: true,
       footer: 'Built with <a href="https://observablehq.com/" target=_blank>Observable</a>',
-      deploy: null
+      deploy: null,
+      telemetry
     });
   });
 });
