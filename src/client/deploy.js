481c9d2536815f821c1bc014ce681180018e59bf
Michael Cooper <mythmon@gmail.com>
2024-01-04 09:35:32 -0800

    buffer unrecognized messages until a listener has attached

--- src/client/deploy.js
+++ src/client/deploy.js
@@ -4,6 +4,7 @@ const origin = process.env.OBSERVABLEHQ_ORIGIN;
 const parent = window.parent; // capture to prevent reassignment
 
 let listener = null;
+let queuedMessages = [];
 
 async function messaged(event) {
   if (!event.isTrusted || event.origin !== origin || event.source !== parent) return;
@@ -16,13 +17,23 @@ async function messaged(event) {
     try {
       if (listener) throw new Error("a script is already loaded");
       const module = await import(message.url);
-      if (module.listener) listener = module.listener;
+      if (module.listener) {
+        listener = module.listener;
+        queuedMessages.forEach((m) => listener(m));
+        queuedMessages = null;
+      }
       postMessage({type: "load_script_complete", url: message.url, re: message.id});
     } catch (error) {
       postMessage({type: "load_script_error", url: message.url, error: error.message, re: message.id});
     }
   } else if (listener) {
     listener(message);
+  } else {
+    if (queuedMessages) {
+      queuedMessages.push(message);
+    } else {
+      console.errror("Bug: tried to enqueue a message after the queue was cleared");
+    }
   }
 }
 
