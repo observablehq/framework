00c2dd110ae6123f2e2d73eac6ea35991b45515b
Michael Cooper <mythmon@gmail.com>
2024-01-10 16:59:06 -0800

    test coverage

--- src/strings.ts
+++ src/strings.ts
@@ -1,14 +1,20 @@
 import type {TtyColor} from "./tty.js";
 import {bold, magenta} from "./tty.js";
 
-export function commandInstruction(command: string, color: TtyColor | null = (s) => magenta(bold(s))): string {
+export function commandInstruction(
+  command: string,
+  {
+    color = (s) => magenta(bold(s)),
+    env = process.env
+  }: {color?: TtyColor | null; env?: Record<string, string | undefined>} = {}
+): string {
   if (!color) color = (s) => s;
 
-  const prefix = process.env["npm_config_user_agent"]?.includes("yarn/")
+  const prefix = env["npm_config_user_agent"]?.includes("yarn/")
     ? "yarn observable"
-    : process.env["npm_config_user_agent"]?.includes("npm/") && process.env["npm_lifecycle_event"] === "npx"
+    : env["npm_config_user_agent"]?.includes("npm/") && env["npm_lifecycle_event"] === "npx"
     ? "npx observable"
-    : process.env["npm_config_user_agent"]?.includes("npm/") && process.env["npm_lifecycle_event"] === "observable"
+    : env["npm_config_user_agent"]?.includes("npm/") && env["npm_lifecycle_event"] === "observable"
     ? "npm run observable"
     : // This probably isn't right, but we've run out of options
       "observable";
