48d9b8e55ba3d1ae5f1bf3c27ce05f7d418b896d
Philippe Rivière <fil@rezo.net>
2024-01-14 08:49:31 -0800

    search index as a data loader, search page

---
+++ docs/data/minisearch.json.ts
@@ -0,0 +1,28 @@
+import {readFile} from "fs/promises";
+import {basename, join} from "path";
+import MiniSearch from "minisearch";
+import {visitMarkdownFiles} from "../../src/files.js"; // TODO proper load
+
+const root = "./docs/"; // TODO proper root
+
+const options = {
+  fields: ["title", "text"], // fields to index for full-text search
+  storeFields: ["title", "category"] // fields to return with search results
+};
+const index = new MiniSearch(options);
+
+for await (const file of visitMarkdownFiles(root)) {
+  if (file === "404.md") continue;
+  console.warn(`Indexing ${file}`);
+  const text = await readFile(join(root, file), "utf-8");
+
+  // TODO h1, front-matter…
+  const title = text.match(/^# (.*)/)?.[1] ?? basename(file, ".md");
+
+  index.add({id: file.slice(0, -3), title, text});
+}
+
+// One way of passing the options to the client; better than nothing, but note
+// that the client can only read the options that are serializable. It's fine
+// for field names, though, which is what we want to share.
+process.stdout.write(JSON.stringify(Object.assign({options}, index.toJSON())));
