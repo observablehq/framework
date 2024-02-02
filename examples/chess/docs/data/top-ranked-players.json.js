930b02470548c7c533397c9b32a73a26779ed609
Philippe Rivière <fil@rezo.net>
2024-02-02 12:23:08 +0100

    * adopt jszip (same as observablehq/stdlib) * adopt fetch * adopt utcMonth * add federation information * wrap file parser

--- examples/chess/docs/data/top-ranked-players.json.js
+++ examples/chess/docs/data/top-ranked-players.json.js
@@ -1,26 +1,30 @@
-import AdmZip from "adm-zip";
-import {range} from "d3-array";
-import request from "request";
+import {utcMonth} from "d3-time";
+import JSZip from "jszip";
 
 const TOP_N_COUNT = 10;
 const MONTHS_OF_DATA = 12;
 
-async function fetchAndFilterTopPlayers() {
-  const today = new Date();
-  const ulrs = range(MONTHS_OF_DATA).map((i) => {
-    const date = new Date(today.getFullYear(), today.getMonth() - i);
-    const year = date.getFullYear().toString().slice(-2);
-    const monthId = date.toLocaleString("en-US", {month: "short"}).toLowerCase();
-    return {
-      month: new Date(date.getFullYear(), date.getMonth(), 1),
-      url: `http://ratings.fide.com/download/standard_${monthId}${year}frl.zip`
-    };
-  });
+function monthlyZipUrl(date) {
+  const id = date
+    .toLocaleString("en-US", {
+      month: "short",
+      year: "2-digit"
+    })
+    .toLowerCase()
+    .replace(" ", "");
+  return `http://ratings.fide.com/download/standard_${id}frl.zip`;
+}
 
+async function fetchAndFilterTopPlayers() {
+  const today = utcMonth();
   const rankingsByMonth = await Promise.all(
-    ulrs.map(({url, month}) =>
-      fetchFideData(url).then((rows) => rows.sort((a, b) => b.rating - a.rating).map((d) => ({...d, month})))
-    )
+    utcMonth
+      .range(utcMonth.offset(today, -11), utcMonth.offset(today, 1))
+      .map((month) =>
+        fetchFideData(monthlyZipUrl(month)).then((rows) =>
+          rows.sort((a, b) => b.rating - a.rating).map((d) => ({...d, month}))
+        )
+      )
   );
 
   // top active women
@@ -37,36 +41,31 @@ async function fetchAndFilterTopPlayers() {
 }
 
 async function fetchFideData(url) {
-  return new Promise((resolve, reject) => {
-    request.get({url, encoding: null}, (err, res, body) => {
-      if (err) reject(err);
-      const zip = new AdmZip(body);
-      resolve(parseFideFile(zip.readAsText(zip.getEntries()[0])));
-    });
-  });
+  return fetch(url)
+    .then((res) => JSZip.loadAsync(res.arrayBuffer()))
+    .then((archive) => archive.file(Object.keys(archive.files).find((name) => name.endsWith(".txt"))).async("text"))
+    .then(parseFideFile);
 }
 
 function parseFideFile(text) {
-  // skip header row
-  return text.split("\n").slice(1).map(parseFideLine);
-}
-
-function parseFideLine(line) {
-  return {
-    id: +line.substring(0, 14).trim(),
-    name: line.substring(15, 75).trim(),
-    // federation: line.substring(76, 79).trim(),
-    sex: line.substring(80, 83).trim(),
-    title: line.substring(84, 88).trim(),
-    // wtit: line.substring(89, 93).trim(),
-    // otit: line.substring(94, 108).trim(),
-    // foa: line.substring(109, 112).trim(),
-    rating: +line.substring(113, 118).trim(),
-    // games: +line.substring(119, 122).trim(),
-    // ratingKFactor: +line.substring(123, 125).trim(),
-    born: +line.substring(126, 131).trim(),
-    flags: line.substring(132, 136).trim()
-  };
+  return text
+    .split("\n")
+    .slice(1) // skip header row
+    .map((line) => ({
+      id: line.substring(0, 14).trim(),
+      name: line.substring(15, 75).trim(),
+      federation: line.substring(76, 79).trim(),
+      sex: line.substring(80, 83).trim(),
+      title: line.substring(84, 88).trim(),
+      // wtit: line.substring(89, 93).trim(),
+      // otit: line.substring(94, 108).trim(),
+      // foa: line.substring(109, 112).trim(),
+      rating: +line.substring(113, 118).trim(),
+      // games: +line.substring(119, 122).trim(),
+      // ratingKFactor: +line.substring(123, 125).trim(),
+      born: +line.substring(126, 131).trim(),
+      flags: line.substring(132, 136).trim()
+    }));
 }
 
 console.log(JSON.stringify(await fetchAndFilterTopPlayers()));
