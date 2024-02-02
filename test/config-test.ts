b7d9f2da8727da49e796ed207795b4baa6ff1b94
Mike Bostock <mbostock@gmail.com>
2024-02-02 15:40:22 -0800

    fix config test

--- test/config-test.ts
+++ test/config-test.ts
@@ -19,6 +19,7 @@ describe("readConfig(undefined, root)", () => {
       title: undefined,
       toc: {label: "On this page", show: true},
       pager: true,
+      head: "",
       header: "",
       footer:
         'Built with <a href="https://observablehq.com/" target="_blank">Observable</a> on <a title="2024-01-11T01:02:03">Jan 11, 2024</a>.',
@@ -37,6 +38,7 @@ describe("readConfig(undefined, root)", () => {
       title: undefined,
       toc: {label: "Contents", show: true},
       pager: true,
+      head: "",
       header: "",
       footer:
         'Built with <a href="https://observablehq.com/" target="_blank">Observable</a> on <a title="2024-01-11T01:02:03">Jan 11, 2024</a>.',
