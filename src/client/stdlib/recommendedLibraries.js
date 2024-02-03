b23c2de2148f2cdd2942220701ea243ead1681f9
Mike Bostock <mbostock@gmail.com>
2024-02-03 10:21:49 -0800

    css

--- src/client/stdlib/recommendedLibraries.js
+++ src/client/stdlib/recommendedLibraries.js
@@ -2,6 +2,7 @@ export const _ = () => import("npm:lodash").then((lodash) => lodash.default);
 export const aq = () => import("npm:arquero");
 export const Arrow = () => import("npm:apache-arrow");
 export const d3 = () => import("npm:d3");
+export const css = () => import("observablehq:stdlib/css").then((css) => css.default); // TODO move to htl?
 export const dot = () => import("observablehq:stdlib/dot").then((dot) => dot.default);
 export const duckdb = () => import("npm:@duckdb/duckdb-wasm");
 export const DuckDBClient = () => import("observablehq:stdlib/duckdb").then((duckdb) => duckdb.DuckDBClient);
