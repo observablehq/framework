841f81a8dc934046ca082aca43f7ec5620331eae
Robert Harris <trebor@observablehq.com>
2024-02-07 10:19:45 -0800

    pretty up the code

--- examples/eia/docs/components/charts.js
+++ examples/eia/docs/components/charts.js
@@ -23,7 +23,7 @@ export function top5BalancingAuthoritiesChart(width, height, top5Demand) {
 
 // US electricity demand, generation and forecasting chart
 export function usGenDemandForecastChart(width, height, usDemandGenForecast, currentHour) {
-    return Plot.plot({
+  return Plot.plot({
     width,
     marginTop: 0,
     height: height - 50,
@@ -48,13 +48,19 @@ export function countryInterchangeChart(width, height, usDemandGenForecast, coun
     width,
     marginTop: 0,
     height: height - 50,
-    color: { legend: true, range: ["#B6B5B1", "#848890"]},
+    color: {legend: true, range: ["#B6B5B1", "#848890"]},
     grid: true,
     y: {label: "GWh exported", labelOffset: 0, tickSize: 0},
-    x: {type: "time", domain: extent(usDemandGenForecast.map(d => d.date)), tickSize: 0, tickPadding: 3},
+    x: {type: "time", domain: extent(usDemandGenForecast.map((d) => d.date)), tickSize: 0, tickPadding: 3},
     marks: [
       Plot.ruleX([currentHour], {strokeOpacity: 0.5}),
-      Plot.areaY(countryInterchangeSeries, {x: "date", y: d => d.value / 1000, curve: "step", fill: "name", tip: true}),
+      Plot.areaY(countryInterchangeSeries, {
+        x: "date",
+        y: (d) => d.value / 1000,
+        curve: "step",
+        fill: "name",
+        tip: true
+      }),
       Plot.ruleY([0], {strokeOpacity: 0.3})
     ]
   });
--- examples/eia/docs/components/map.js
+++ examples/eia/docs/components/map.js
@@ -1,6 +1,7 @@
 import {extent} from "npm:d3-array";
 import {format} from "npm:d3-format";
 import * as Plot from "npm:@observablehq/plot";
+import {html} from "npm:htl";
 
 // Colors and scale
 const colorGenerating = "#88DCAD";
@@ -24,100 +25,102 @@ export function balancingAuthoritiesMap({
   statemesh,
   width
 }) {
-  return Plot.plot({
-    width: Math.min(width, 620),
-    height: Math.min(width, 620) * 0.6,
-    color: {
-      ...color,
-      transform: (d) => d / 100,
-      label: "Change in demand (%) from previous hour"
-    },
-    projection: {
-      type: "albers",
-      insetTop: 15
-    },
-    r: {
-      domain: extent(eiaPoints, (d) => d.radius),
-      range: [4, 30]
-    },
-    marks: [
-      Plot.geo(nation, {fill: "currentColor", fillOpacity: 0.1, stroke: "var(--theme-background-alt)"}),
-      Plot.geo(statemesh, {stroke: "var(--theme-background-alt)", strokeWidth: 0.8}),
-      Plot.arrow(eiaConnRefSpatial, {
-        filter: (d) => d.location1[0] > d.location2[0],
-        x1: (d) => d.location1[0],
-        y1: (d) => d.location1[1],
-        x2: (d) => d.location2[0],
-        y2: (d) => d.location2[1],
-        stroke: "currentColor",
-        strokeWidth: 0.5,
-        opacity: 0.7,
-        bend: 7,
-        headLength: 0
-      }),
-      Plot.dot(eiaPoints, {
-        x: "lon",
-        y: "lat",
-        r: "radius",
-        stroke: "gray",
-        strokeWidth: 1,
-        filter: (d) => isNaN(baHourlyChange.get(d.name)) && !(d.region_id === "MEX" || d.region_id === "CAN"),
-        fill: "#6D6D6D"
-      }),
-      Plot.dot(eiaPoints, {
-        x: "lon",
-        y: "lat",
-        r: 4,
-        symbol: "square",
-        stroke: "gray",
-        strokeWidth: 1,
-        filter: (d) => d.region_id === "MEX" || d.region_id === "CAN",
-        fill: "#6D6D6D"
-      }),
-      Plot.dot(eiaPoints, {
-        filter: (d) => genOnlyBA.includes(d.id),
-        x: "lon",
-        y: "lat",
-        r: "radius",
-        fill: colorGenerating,
-        stroke: "gray",
-        strokeWidth: 1
-      }),
-      Plot.dot(eiaPoints, {
-        x: "lon",
-        y: "lat",
-        r: "radius",
-        stroke: colorUnavailable,
-        strokeWidth: 1,
-        filter: (d) => !isNaN(baHourlyChange.get(d.name)),
-        fill: (d) => baHourlyChange.get(d.name)
-      }),
-      Plot.text(eiaPoints, {
-        x: "lon",
-        y: "lat",
-        text: (d) => (d.radius > 10000 ? d.id : null),
-        fontWeight: 800,
-        fill: "black"
-      }),
-      Plot.tip(
-        eiaPoints,
-        Plot.pointer({
+  return html`<div style="display: flex; justify-content: center">${
+    Plot.plot({
+      width: Math.min(width, 620),
+      height: Math.min(width, 620) * 0.6,
+      color: {
+        ...color,
+        transform: (d) => d / 100,
+        label: "Change in demand (%) from previous hour"
+      },
+      projection: {
+        type: "albers",
+        insetTop: 15
+      },
+      r: {
+        domain: extent(eiaPoints, (d) => d.radius),
+        range: [4, 30]
+      },
+      marks: [
+        Plot.geo(nation, {fill: "currentColor", fillOpacity: 0.1, stroke: "var(--theme-background-alt)"}),
+        Plot.geo(statemesh, {stroke: "var(--theme-background-alt)", strokeWidth: 0.8}),
+        Plot.arrow(eiaConnRefSpatial, {
+          filter: (d) => d.location1[0] > d.location2[0],
+          x1: (d) => d.location1[0],
+          y1: (d) => d.location1[1],
+          x2: (d) => d.location2[0],
+          y2: (d) => d.location2[1],
+          stroke: "currentColor",
+          strokeWidth: 0.5,
+          opacity: 0.7,
+          bend: 7,
+          headLength: 0
+        }),
+        Plot.dot(eiaPoints, {
           x: "lon",
           y: "lat",
-          title: (d) =>
-            d.region_id === "MEX" || d.region_id === "CAN"
-              ? d.name
-              : `${d.name} (${d.id})\nChange from previous hour: ${
-                  isNaN(baHourlyChange.get(d.name)) ? "Unavailable" : baHourlyChange.get(d.name).toFixed(1) + "%"
-                }\nLatest hourly demand: ${
-                  isNaN(baHourlyLatest.get(d.name))
-                    ? "Unavailable"
-                    : (baHourlyLatest.get(d.name) / 1000).toFixed(2) + " GWh"
-                }`
-        })
-      )
-    ]
-  });
+          r: "radius",
+          stroke: "gray",
+          strokeWidth: 1,
+          filter: (d) => isNaN(baHourlyChange.get(d.name)) && !(d.region_id === "MEX" || d.region_id === "CAN"),
+          fill: "#6D6D6D"
+        }),
+        Plot.dot(eiaPoints, {
+          x: "lon",
+          y: "lat",
+          r: 4,
+          symbol: "square",
+          stroke: "gray",
+          strokeWidth: 1,
+          filter: (d) => d.region_id === "MEX" || d.region_id === "CAN",
+          fill: "#6D6D6D"
+        }),
+        Plot.dot(eiaPoints, {
+          filter: (d) => genOnlyBA.includes(d.id),
+          x: "lon",
+          y: "lat",
+          r: "radius",
+          fill: colorGenerating,
+          stroke: "gray",
+          strokeWidth: 1
+        }),
+        Plot.dot(eiaPoints, {
+          x: "lon",
+          y: "lat",
+          r: "radius",
+          stroke: colorUnavailable,
+          strokeWidth: 1,
+          filter: (d) => !isNaN(baHourlyChange.get(d.name)),
+          fill: (d) => baHourlyChange.get(d.name)
+        }),
+        Plot.text(eiaPoints, {
+          x: "lon",
+          y: "lat",
+          text: (d) => (d.radius > 10000 ? d.id : null),
+          fontWeight: 800,
+          fill: "black"
+        }),
+        Plot.tip(
+          eiaPoints,
+          Plot.pointer({
+            x: "lon",
+            y: "lat",
+            title: (d) =>
+              d.region_id === "MEX" || d.region_id === "CAN"
+                ? d.name
+                : `${d.name} (${d.id})\nChange from previous hour: ${
+                    isNaN(baHourlyChange.get(d.name)) ? "Unavailable" : baHourlyChange.get(d.name).toFixed(1) + "%"
+                  }\nLatest hourly demand: ${
+                    isNaN(baHourlyLatest.get(d.name))
+                      ? "Unavailable"
+                      : (baHourlyLatest.get(d.name) / 1000).toFixed(2) + " GWh"
+                  }`
+          })
+        )
+      ]
+    })
+  }</div>`;
 }
 
 // Map legend
