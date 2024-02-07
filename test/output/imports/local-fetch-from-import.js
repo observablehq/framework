d9bca1f2a06cfcbfa46da2cc89d5f5f88cfeceb9
Mike Bostock <mbostock@gmail.com>
2024-02-06 21:49:52 -0800

    set mtime, again

--- test/output/imports/local-fetch-from-import.js
+++ test/output/imports/local-fetch-from-import.js
@@ -1,4 +1,4 @@
-define({id: "0", inputs: ["display"], outputs: ["data"], files: [{"name":"./fetch-local-data.json","mimeType":"application/json","path":"./_file/fetch-local-data.json","lastModified":1701883559359}], body: async (display) => {
+define({id: "0", inputs: ["display"], outputs: ["data"], files: [{"name":"./fetch-local-data.json","mimeType":"application/json","path":"./_file/fetch-local-data.json","lastModified":1729072800000}], body: async (display) => {
 const {data} = await import("./_import/baz.js?sha=065e1149636e2894e471ad52d5be266d0b2ff7bbd7d982832d09f7b367ff8480");
 
 display(data);
