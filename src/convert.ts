ba7538314a3d7d3a0d085382a2f402c3d910a829
Mike Bostock <mbostock@gmail.com>
2024-02-12 16:49:07 -0800

    observable convert

---
+++ src/convert.ts
@@ -0,0 +1,46 @@
+import {getObservableUiOrigin} from "./observableApiClient.js";
+
+export async function convert(inputs: string[]): Promise<void> {
+  for (const input of inputs.map(resolveInput)) {
+    const response = await fetch(input);
+    if (!response.ok) throw new Error(`error fetching ${input}: ${response.status}`);
+    process.stdout.write(convertNodes((await response.json()).nodes));
+  }
+}
+
+export function convertNodes(nodes): string {
+  let string = "";
+  let first = true;
+  for (const node of nodes) {
+    if (first) first = false;
+    else string += "\n";
+    string += convertNode(node);
+  }
+  return string;
+}
+
+export function convertNode(node): string {
+  let string = "";
+  if (node.mode !== "md") string += `\`\`\`${node.mode}${node.pinned ? " echo" : ""}\n`;
+  string += `${node.value}\n`;
+  if (node.mode !== "md") string += "```\n";
+  return string;
+}
+
+export function resolveInput(input: string): string {
+  let url: URL;
+  if (isIdSpecifier(input)) url = new URL(`/d/${input}`, getObservableUiOrigin());
+  else if (isSlugSpecifier(input)) url = new URL(`/${input}`, getObservableUiOrigin());
+  else url = new URL(input);
+  url.host = `api.${url.host}`;
+  url.pathname = `/document${url.pathname.replace(/^\/d\//, "/")}`;
+  return String(url);
+}
+
+function isIdSpecifier(string: string) {
+  return /^([0-9a-f]{16})(?:@(\d+)|~(\d+)|@(\w+))?$/.test(string);
+}
+
+function isSlugSpecifier(string: string) {
+  return /^(?:@([0-9a-z_-]+))\/([0-9a-z_-]+(?:\/[0-9]+)?)(?:@(\d+)|~(\d+)|@(\w+))?$/.test(string);
+}
