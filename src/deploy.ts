695a55d9f51d1c1fc977c0cba73a8c8d2beca717
Sylvestre Gug <sylvestre@observablehq.com>
2024-02-14 09:20:38 -0500

    pattern

--- src/deploy.ts
+++ src/deploy.ts
@@ -104,12 +104,11 @@ export async function deploy(
   let authError: null | "unauthenticated" | "forbidden" = null;
   try {
     if (apiKey) {
+      const memberTiers = new Set(["starter_2024", "pro_2024", "enterprise_2024"]);
       currentUser = await apiClient.getCurrentUser();
       // List of valid workspaces that can be used to create projects.
       currentUser.workspaces = currentUser.workspaces.filter(
-        (w) =>
-          (w.role === "owner" || w.role === "member") &&
-          ["starter_2024", "pro_2024", "enterprise_2024"].includes(w.tier)
+        (w) => (w.role === "owner" || w.role === "member") && memberTiers.has(w.tier)
       );
     }
   } catch (error) {
