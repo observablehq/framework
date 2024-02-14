8121a6a17f2f4b4aa4a3c24d295d3c4d1664c552
Mike Bostock <mbostock@gmail.com>
2024-02-14 11:17:48 -0800

    more polish

--- src/client/search.js
+++ src/client/search.js
@@ -1,14 +1,18 @@
 import MiniSearch from "minisearch";
 
 const container = document.querySelector("#observablehq-search");
+const sidebar = document.querySelector("#observablehq-sidebar");
 const shortcut = container.getAttribute("data-shortcut");
 const input = container.querySelector("input");
-const r = document.querySelector("#observablehq-search-results");
-const c = "observablehq-link-active";
-let value;
+const resultsContainer = document.querySelector("#observablehq-search-results");
+const activeClass = "observablehq-link-active";
+let currentValue;
 
 const index = await fetch(import.meta.resolve("./minisearch.json"))
-  .then((resp) => resp.json())
+  .then((response) => {
+    if (!response.ok) throw new Error(`unable to load minisearch.json: ${response.status}`);
+    return response.json();
+  })
   .then((json) =>
     MiniSearch.loadJS(json, {
       ...json.options,
@@ -17,58 +21,61 @@ const index = await fetch(import.meta.resolve("./minisearch.json"))
   );
 
 input.addEventListener("input", () => {
-  if (value === input.value) return;
-  value = input.value;
-  if (!value.length) {
+  if (currentValue === input.value) return;
+  currentValue = input.value;
+  if (!currentValue.length) {
     container.setAttribute("data-shortcut", shortcut);
-    container.parentElement.classList.remove("observablehq-search-results");
-    r.innerHTML = "";
+    sidebar.classList.remove("observablehq-search-results");
+    resultsContainer.innerHTML = "";
     return;
   }
   container.setAttribute("data-shortcut", ""); // prevent conflict with close button
-  container.parentElement.classList.add("observablehq-search-results");
-  const results = index.search(value, {boost: {title: 4}, fuzzy: 0.15, prefix: true}).slice(0, 11);
-  r.innerHTML =
+  sidebar.classList.add("observablehq-search-results"); // hide pages while showing search results
+  const results = index.search(currentValue, {boost: {title: 4}, fuzzy: 0.15, prefix: true});
+  resultsContainer.innerHTML =
     results.length === 0
       ? "<div>no results</div>"
-      : `<div>${results.length === 11 ? "10+" : results.length} result${
-          results.length === 1 ? "" : "s"
-        }</div><ol>${"<li><a></a></li>".repeat(results.length)}</ol>`;
-  r.querySelectorAll("li").forEach((li, i) => {
-    const {id, score, title} = results[i];
-    li.setAttribute("class", `observablehq-link${i === 0 ? ` ${c}` : ""}`);
-    li.setAttribute("data-score", Math.min(5, Math.round(0.6 * score)));
-    const a = li.firstChild;
-    a.setAttribute("href", import.meta.resolve(`../${id}`));
-    a.textContent = title;
-  });
+      : `<div>${results.length.toLocaleString("en-US")} result${results.length === 1 ? "" : "s"}</div><ol>${results
+          .map(renderResult)
+          .join("")}</ol>`;
 });
+
+function renderResult({id, score, title}, i) {
+  return `<li data-score="${Math.min(5, Math.round(0.6 * score))}" class="observablehq-link${
+    i === 0 ? ` ${activeClass}` : ""
+  }"><a href="${escapeDoubleQuote(import.meta.resolve(`../${id}`))}">${escapeText(title)}</a></li>`;
+}
+
+function escapeDoubleQuote(text) {
+  return text.replace(/["&]/g, entity);
+}
+
+function escapeText(text) {
+  return text.replace(/[<&]/g, entity);
+}
+
+function entity(character) {
+  return `&#${character.charCodeAt(0).toString()};`;
+}
 
 // Handle a race condition where an input event fires while awaiting the index fetch.
 input.dispatchEvent(new Event("input"));
 
-addEventListener("keydown", (event) => {
-  const {code, target} = event;
-  if (target === input) {
-    if (code === "Escape" && input.value === "") {
-      input.blur();
-      return;
-    }
-    if (code === "ArrowDown" || code === "ArrowUp" || code === "Enter") {
-      const results = r.querySelector("ol");
-      const current = results?.querySelector(`.${c}`);
-      if (!current) return;
-      if (code === "Enter") {
-        current.querySelector("a")?.click();
-      } else {
-        if (code === "ArrowUp") {
-          current.classList.remove(c);
-          (current.previousElementSibling ?? results.querySelector("li:last-child"))?.classList.add(c);
-        } else if (code === "ArrowDown") {
-          current.classList.remove(c);
-          (current.nextElementSibling ?? results.querySelector("li:first-child")).classList.add(c);
-        }
-      }
+input.addEventListener("keydown", (event) => {
+  const {code} = event;
+  if (code === "Escape" && input.value === "") return input.blur();
+  if (code === "ArrowDown" || code === "ArrowUp" || code === "Enter") {
+    const results = resultsContainer.querySelector("ol");
+    if (!results) return;
+    const activeResult = results.querySelector(`.${activeClass}`);
+    if (code === "Enter") {
+      activeResult.querySelector("a").click();
+    } else if (code === "ArrowUp") {
+      activeResult.classList.remove(activeClass);
+      (activeResult.previousElementSibling ?? results.lastElementChild).classList.add(activeClass);
+    } else if (code === "ArrowDown") {
+      activeResult.classList.remove(activeClass);
+      (activeResult.nextElementSibling ?? results.firstElementChild).classList.add(activeClass);
     }
   }
 });
