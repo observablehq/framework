2ecad1937fa4b4435f2f0a27ed410995e93daefa
Michael Cooper <mythmon@gmail.com>
2024-02-22 15:19:28 -0800

    better handle paths in create

--- bin/observable-init.js
+++ bin/observable-init.js
@@ -1,4 +1,10 @@
-#!/usr/bin/env -S node --no-warnings=ExperimentalWarning
+#!/usr/bin/env node
+import {fileURLToPath} from "node:url";
+import crossSpawn from "cross-spawn";
 
-await import("tsx/esm");
-await import("./observable.ts");
+const observablePath = fileURLToPath(import.meta.resolve("./observable.ts"));
+crossSpawn.sync(
+  "node",
+  ["--no-warnings=ExperimentalWarning", "--import", "tsx/esm", observablePath, ...process.argv.slice(2)],
+  {stdio: "inherit"}
+);
