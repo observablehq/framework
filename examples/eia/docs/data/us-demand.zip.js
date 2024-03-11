d98b13c8f33b4d7db1b838ae05b7af17cc019fa4
Jeff Pettiross <jeff@observablehq.com>
2024-03-11 16:53:23 -0700

    Add semicolons

--- examples/eia/docs/data/us-demand.zip.js
+++ examples/eia/docs/data/us-demand.zip.js
@@ -1,16 +1,16 @@
-import * as d3 from "d3"
-import JSZip from "jszip"
+import * as d3 from "d3";
+import JSZip from "jszip";
 
 // Construct web API call for last 7 days of hourly demand data in MWh
 // Types: DF = forecasted demand, D = demand (actual), NG = net generation
-const end = d3.timeDay.offset(d3.timeHour(new Date()), 1)
-const start = d3.timeHour(d3.utcDay.offset(end, -7))
-const convertDate = d3.timeFormat("%m%d%Y %H:%M:%S")
-const usDemandUrl = `https://www.eia.gov/electricity/930-api/region_data/series_data?type[0]=DF&type[1]=D&type[2]=NG&start=${convertDate(start)}&end=${convertDate(end)}&frequency=hourly&timezone=Eastern&limit=10000&respondent[0]=US48`
+const end = d3.timeDay.offset(d3.timeHour(new Date()), 1);
+const start = d3.timeHour(d3.utcDay.offset(end, -7));
+const convertDate = d3.timeFormat("%m%d%Y %H:%M:%S");
+const usDemandUrl = `https://www.eia.gov/electricity/930-api/region_data/series_data?type[0]=DF&type[1]=D&type[2]=NG&start=${convertDate(start)}&end=${convertDate(end)}&frequency=hourly&timezone=Eastern&limit=10000&respondent[0]=US48`;
 
-const datetimeFormat = d3.utcParse("%m/%d/%Y %H:%M:%S")
-const dateFormat = d3.utcParse("%m/%d/%Y")
-const typeNameRemap = {DF: "demandForecast", D: "demandActual", NG: "netGeneration"}
+const datetimeFormat = d3.utcParse("%m/%d/%Y %H:%M:%S");
+const dateFormat = d3.utcParse("%m/%d/%Y");
+const typeNameRemap = {DF: "demandForecast", D: "demandActual", NG: "netGeneration"};
 
 // Flatten JSON from date / type / value hierarchy to a tidy array
 const jsonToTidy = (data, id) => {
@@ -24,7 +24,7 @@ const jsonToTidy = (data, id) => {
       }
     })
   })
-}
+};
 
 // Roll up each hour's values into a single row for a cohesive tip
 function tidyToRollup(data) {
@@ -39,12 +39,12 @@ function tidyToRollup(data) {
   return rolledUpSparse
 }
 
-const jsonData = await d3.json(usDemandUrl)
-const tidySeries = jsonToTidy(jsonData, "TYPE_ID")
-const summaryData = tidyToRollup(tidySeries)
+const jsonData = await d3.json(usDemandUrl);
+const tidySeries = jsonToTidy(jsonData, "TYPE_ID");
+const summaryData = tidyToRollup(tidySeries);
 
 const zip = new JSZip();
-zip.file("summary.csv", d3.csvFormat(summaryData))
-zip.file("detail.csv", d3.csvFormat(tidySeries))
-zip.generateNodeStream().pipe(process.stdout)
+zip.file("summary.csv", d3.csvFormat(summaryData));
+zip.file("detail.csv", d3.csvFormat(tidySeries));
+zip.generateNodeStream().pipe(process.stdout);
 
