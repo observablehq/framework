b0a651b01baee01ed08d23841174c5f469794d7d
Mike Bostock <mbostock@gmail.com>
2024-03-12 21:57:35 -0700

    more tests

--- test/typescript-test.ts
+++ test/typescript-test.ts
@@ -2,22 +2,40 @@ import assert from "node:assert";
 import {getTypeScriptPath, transpileTypeScript} from "../src/typescript.js";
 
 describe("transpileTypeScript(input)", () => {
-  it("basic", () => {
-    assert.strictEqual(transpileTypeScript("1 + 2"), "1 + 2");
+  it("transpiles an arthimetic expression", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number"), "1 + 2");
   });
-  it("empty", () => {
+  it("transpiles an arthimetic expression with whitespace", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number "), "1 + 2");
+  });
+  it("transpiles an arthimetic expression with comment", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number // comment"), "1 + 2");
+  });
+  it("preserves the trailing semicolon", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number;"), "1 + 2;\n");
+  });
+  it("preserves the trailing semicolon with whitepsace", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number; "), "1 + 2;\n");
+  });
+  it.skip("preserves the trailing semicolon with comment", () => {
+    assert.strictEqual(transpileTypeScript("1 + 2 as number; // comment"), "1 + 2;\n");
+  });
+  it("transpiles empty input", () => {
     assert.strictEqual(transpileTypeScript(""), "");
   });
-  it("function call", () => {
-    assert.strictEqual(transpileTypeScript("sum(1, 2)"), "sum(1, 2)");
+  it("transpiles a function call", () => {
+    assert.strictEqual(transpileTypeScript("sum(1, 2 as number)"), "sum(1, 2)");
   });
-  it("single import", () => {
-    assert.strictEqual(transpileTypeScript('import {sum} from "./sum.ts";'), 'import { sum } from "./sum.ts";\n');
+  it("transpiles an import", () => {
+    assert.strictEqual(transpileTypeScript('import {sum} from "./sum.js";'), 'import { sum } from "./sum.js";\n');
   });
-  it("import and value", () => {
+  it("transpiles a type import", () => {
+    assert.strictEqual(transpileTypeScript('import type {sum} from "./sum.js";'), "");
+  });
+  it("transpiles an import and statement", () => {
     assert.strictEqual(
-      transpileTypeScript('import {sum} from "./sum.ts"; sum(1, 2)'),
-      'import { sum } from "./sum.ts";\nsum(1, 2)'
+      transpileTypeScript('import {sum} from "./sum.js"; sum(1, 2);'),
+      'import { sum } from "./sum.js";\nsum(1, 2);\n'
     );
   });
 });
