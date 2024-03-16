429b1f38cff5b5be6204229a49c32a56cca2e072
Mike Bostock <mbostock@gmail.com>
2024-03-15 20:46:20 -0700

    normalize file path; deterministic resolution (#1076)
    * normalize file path; deterministic file resolution
    
    * getSourceFileHash
    
    * fix archives.win32 snapshot?

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -12,6 +12,7 @@ import {extractNpmSpecifier, populateNpmCache, resolveNpmImport, resolveNpmImpor
 import {isAssetPath, isPathImport, relativePath, resolveLocalPath, resolvePath} from "./path.js";
 
 export interface Resolvers {
+  path: string;
   hash: string;
   assets: Set<string>; // like files, but not registered for FileAttachment
   files: Set<string>;
@@ -115,10 +116,10 @@ export async function getResolvers(
   }
 
   // Compute the content hash.
-  for (const f of assets) hash.update(loaders.getFileHash(resolvePath(path, f)));
-  for (const f of files) hash.update(loaders.getFileHash(resolvePath(path, f)));
+  for (const f of assets) hash.update(loaders.getSourceFileHash(resolvePath(path, f)));
+  for (const f of files) hash.update(loaders.getSourceFileHash(resolvePath(path, f)));
   for (const i of localImports) hash.update(getModuleHash(root, resolvePath(path, i)));
-  if (page.style && isPathImport(page.style)) hash.update(loaders.getFileHash(resolvePath(path, page.style)));
+  if (page.style && isPathImport(page.style)) hash.update(loaders.getSourceFileHash(resolvePath(path, page.style)));
 
   // Collect transitively-attached files and local imports.
   for (const i of localImports) {
@@ -259,6 +260,7 @@ export async function getResolvers(
   }
 
   return {
+    path,
     hash: hash.digest("hex"),
     assets,
     files,
