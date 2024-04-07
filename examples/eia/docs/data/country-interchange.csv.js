a872981c9ebe1c03443b4b2f3788955fd8a5374f
Allison Horst <ahorst@ucsb.edu>
2024-04-07 08:58:53 -0600

    convert eia to static

--- examples/eia/docs/data/country-interchange.csv.js
+++ examples/eia/docs/data/country-interchange.csv.js
@@ -1,21 +1,17 @@
 import * as d3 from "d3";
-
-const end = d3.timeDay.offset(d3.timeHour(new Date()), 1);
-const start = d3.timeHour(d3.utcDay.offset(end, -7));
-const convertDate = d3.timeFormat("%m%d%Y %H:%M:%S"); // Convert dates into the format the EIA web API expects
+import {readFileSync} from "node:fs";
 
 // Electricity exported (in GWh) from the lower 48 US states to Canada and Mexico
-const countryInterchangeUrl = `https://www.eia.gov/electricity/930-api/interchange/series_data?type[0]=TI&start=${convertDate(start)}&end=${convertDate(end)}&frequency=hourly&from_respondent[0]=US48&timezone=Eastern&limit=10000&offset=0`;
 
-const tidySeries = (response, id, name) => {
+const tidySeries = (response) => {
   let series = response[0].data;
   let datetimeFormat = d3.utcParse("%m/%d/%Y %H:%M:%S");
   let dateFormat = d3.utcParse("%m/%d/%Y");
   return series.flatMap((s) => {
     return s.VALUES.DATES.map((d, i) => {
       return {
-        id: s[id],
-        name: s[name],
+        id: s["TO_RESPONDENT_ID"],
+        name: s["TO_RESPONDENT_NAME"],
         date: datetimeFormat(d) ?? dateFormat(d),
         value: s.VALUES.DATA[i]
       };
@@ -23,8 +19,10 @@ const tidySeries = (response, id, name) => {
   });
 };
 
-const countryInterchangeSeries = await d3.json(countryInterchangeUrl).then((response) => {
-  return tidySeries(response, "TO_RESPONDENT_ID", "TO_RESPONDENT_NAME");
-});
+const countryInterchangeSeriesJSON = JSON.parse(
+  readFileSync("docs/data/countryInterchangeSeries-20240328-20240403.json")
+);
+
+const countryInterchangeSeries = tidySeries(countryInterchangeSeriesJSON);
 
 process.stdout.write(d3.csvFormat(countryInterchangeSeries));
