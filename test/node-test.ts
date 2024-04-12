fcd2caaf79c4c83504541d566ad63a1f6c8980b8
Mike Bostock <mbostock@gmail.com>
2024-04-12 08:39:56 -0700

    more test isolation

--- test/node-test.ts
+++ test/node-test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert";
-import {existsSync} from "node:fs";
 import {rm} from "node:fs/promises";
+import {join} from "node:path/posix";
 import {extractNodeSpecifier, isNodeBuiltin, resolveNodeImport, resolveNodeImports} from "../src/node.js";
 
 describe("isNodeBuiltin(specifier)", () => {
@@ -29,64 +29,63 @@ describe("isNodeBuiltin(specifier)", () => {
   });
 });
 
-describe("resolveNodeImport(root, spec)", () => {
-  const importRoot = "../../input/packages/.observablehq/cache";
-  before(async () => {
-    await rm("docs/.observablehq/cache/_node", {recursive: true, force: true});
-    await rm("test/input/packages/.observablehq/cache", {recursive: true, force: true});
-  });
+describe("resolveNodeImport(root, spec) with top-level node_modules", () => {
+  const testRoot = "test/output/node-import"; // unique to this test
+  before(() => rm(join(testRoot, ".observablehq/cache/_node"), {recursive: true, force: true}));
   it("resolves the version of a direct dependency", async () => {
-    assert.deepStrictEqual(await resolveNodeImport("docs", "d3-array"), "/_node/d3-array@3.2.4/index.js");
-    assert.deepStrictEqual(await resolveNodeImport("docs", "mime"), "/_node/mime@4.0.1/index.js");
+    assert.deepStrictEqual(await resolveNodeImport(testRoot, "d3-array"), "/_node/d3-array@3.2.4/index.js");
+    assert.deepStrictEqual(await resolveNodeImport(testRoot, "mime"), "/_node/mime@4.0.1/index.js");
   });
   it("allows entry points", async () => {
-    assert.deepStrictEqual(await resolveNodeImport("docs", "mime/lite"), "/_node/mime@4.0.1/lite.js");
+    assert.deepStrictEqual(await resolveNodeImport(testRoot, "mime/lite"), "/_node/mime@4.0.1/lite.js");
   });
   it("allows non-javascript entry points", async () => {
-    assert.deepStrictEqual(await resolveNodeImport("docs", "glob/package.json"), "/_node/glob@10.3.10/package.json");
+    assert.deepStrictEqual(await resolveNodeImport(testRoot, "glob/package.json"), "/_node/glob@10.3.10/package.json");
   });
   it("does not allow version ranges", async () => {
-    await assert.rejects(() => resolveNodeImport("docs", "mime@4"), /Cannot find module/);
+    await assert.rejects(() => resolveNodeImport(testRoot, "mime@4"), /Cannot find module/);
   });
+});
+
+describe("resolveNodeImport(root, spec) with test node_modules", () => {
+  const testRoot = "test/input/packages"; // unique to this test
+  const importRoot = "../../input/packages/.observablehq/cache";
+  before(() => rm(join(testRoot, ".observablehq/cache/_node"), {recursive: true, force: true}));
   it("bundles a package with a shorthand export", async () => {
-    assert.strictEqual(await resolveNodeImport("test/input/packages", "test-shorthand-export"), "/_node/test-shorthand-export@1.0.0/index.js"); // prettier-ignore
+    assert.strictEqual(await resolveNodeImport(testRoot, "test-shorthand-export"), "/_node/test-shorthand-export@1.0.0/index.js"); // prettier-ignore
     assert.strictEqual((await import(`${importRoot}/_node/test-shorthand-export@1.0.0/index.js`)).name, "test-shorthand-export"); // prettier-ignore
   });
   it("bundles a package with an import conditional export", async () => {
-    assert.strictEqual(await resolveNodeImport("test/input/packages", "test-import-condition"), "/_node/test-import-condition@1.0.0/index.js"); // prettier-ignore
+    assert.strictEqual(await resolveNodeImport(testRoot, "test-import-condition"), "/_node/test-import-condition@1.0.0/index.js"); // prettier-ignore
     assert.strictEqual((await import(`${importRoot}/_node/test-import-condition@1.0.0/index.js`)).name, "test-import-condition:import"); // prettier-ignore
   });
   it("bundles a package with a browser field", async () => {
-    assert.strictEqual(await resolveNodeImport("test/input/packages", "test-browser-field"), "/_node/test-browser-field@1.0.0/index.js"); // prettier-ignore
+    assert.strictEqual(await resolveNodeImport(testRoot, "test-browser-field"), "/_node/test-browser-field@1.0.0/index.js"); // prettier-ignore
     assert.strictEqual((await import(`${importRoot}/_node/test-browser-field@1.0.0/index.js`)).name, "test-browser-field:browser"); // prettier-ignore
   });
   it("bundles a package with a browser map", async () => {
-    assert.strictEqual(await resolveNodeImport("test/input/packages", "test-browser-map"), "/_node/test-browser-map@1.0.0/index.js"); // prettier-ignore
+    assert.strictEqual(await resolveNodeImport(testRoot, "test-browser-map"), "/_node/test-browser-map@1.0.0/index.js"); // prettier-ignore
     assert.strictEqual((await import(`${importRoot}/_node/test-browser-map@1.0.0/index.js`)).name, "test-browser-map:browser"); // prettier-ignore
   });
   it("bundles a package with a browser conditional export", async () => {
-    assert.strictEqual(await resolveNodeImport("test/input/packages", "test-browser-condition"), "/_node/test-browser-condition@1.0.0/index.js"); // prettier-ignore
+    assert.strictEqual(await resolveNodeImport(testRoot, "test-browser-condition"), "/_node/test-browser-condition@1.0.0/index.js"); // prettier-ignore
     assert.strictEqual((await import(`${importRoot}/_node/test-browser-condition@1.0.0/index.js`)).name, "test-browser-condition:browser"); // prettier-ignore
   });
 });
 
 describe("resolveNodeImports(root, path)", () => {
-  before(async () => {
-    if (existsSync("docs/.observablehq/cache/_node")) {
-      await rm("docs/.observablehq/cache/_node", {recursive: true});
-    }
-  });
+  const testRoot = "test/output/node-imports"; // unique to this test
+  before(() => rm(join(testRoot, ".observablehq/cache/_node"), {recursive: true, force: true}));
   it("resolves the imports of a dependency", async () => {
-    assert.deepStrictEqual(await resolveNodeImports("docs", await resolveNodeImport("docs", "d3-array")), [
-      {
-        method: "static",
-        name: "../internmap@2.0.3/index.js",
-        type: "local"
-      }
+    assert.deepStrictEqual(await resolveNodeImports(testRoot, await resolveNodeImport(testRoot, "d3-array")), [
+      {method: "static", name: "../internmap@2.0.3/index.js", type: "local"}
     ]);
   });
   it("ignores non-JavaScript paths", async () => {
-    assert.deepStrictEqual(await resolveNodeImports("docs", await resolveNodeImport("docs", "glob/package.json")), []);
+    assert.deepStrictEqual(
+      await resolveNodeImports(testRoot, await resolveNodeImport(testRoot, "glob/package.json")),
+      []
+    );
   });
 });
 
--- test/resolvers-test.ts
+++ test/resolvers-test.ts
@@ -5,82 +5,82 @@ import {parseMarkdown} from "../src/markdown.js";
 import {getResolvers} from "../src/resolvers.js";
 import {mockJsDelivr} from "./mocks/jsdelivr.js";
 
-async function getOptions({root, path}: {root: string; path: string}): Promise<Config & {path: string}> {
-  return {...(await normalizeConfig({root})), path};
+function getOptions({root, path}: {root: string; path: string}): Config & {path: string} {
+  return {...normalizeConfig({root}), path};
 }
 
 describe("getResolvers(page, {root, path})", () => {
   mockJsDelivr();
   const builtins = ["npm:@observablehq/runtime", "npm:@observablehq/stdlib", "observablehq:client"];
   it("resolves directly-attached files", async () => {
-    const options = await getOptions({root: "test/input", path: "attached.md"});
+    const options = getOptions({root: "test/input", path: "attached.md"});
     const page = parseMarkdown("${FileAttachment('foo.csv')}", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.files, new Set(["./foo.csv"]));
   });
   it("ignores files that are outside of the source root", async () => {
-    const options = await getOptions({root: "test/input", path: "attached.md"});
+    const options = getOptions({root: "test/input", path: "attached.md"});
     const page = parseMarkdown("${FileAttachment('../foo.csv')}", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.files, new Set([]));
   });
   it("detects file methods", async () => {
-    const options = await getOptions({root: "test/input", path: "attached.md"});
+    const options = getOptions({root: "test/input", path: "attached.md"});
     const page = parseMarkdown("${FileAttachment('foo.csv').csv}", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(["npm:d3-dsv", ...builtins]));
   });
   it("detects local static imports", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport './bar.js';\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(["./bar.js", ...builtins]));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./bar.js"]));
   });
   it("detects local transitive static imports", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport './other/foo.js';\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(["./other/foo.js", "./bar.js", ...builtins]));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./other/foo.js", "./bar.js"]));
   });
   it("detects local transitive static imports (2)", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport './transitive-static-import.js';\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(["./transitive-static-import.js", "./other/foo.js", "./bar.js", ...builtins])); // prettier-ignore
     assert.deepStrictEqual(resolvers.localImports, new Set(["./transitive-static-import.js", "./other/foo.js", "./bar.js"])); // prettier-ignore
   });
   it("detects local transitive dynamic imports", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport './dynamic-import.js';\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(["./dynamic-import.js", ...builtins]));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./dynamic-import.js", "./bar.js"]));
   });
   it("detects local transitive dynamic imports (2)", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport('./dynamic-import.js');\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(builtins));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./dynamic-import.js", "./bar.js"]));
   });
   it("detects local transitive dynamic imports (3)", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport('./transitive-dynamic-import.js');\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(builtins));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./transitive-dynamic-import.js", "./other/foo.js", "./bar.js"])); // prettier-ignore
   });
   it("detects local transitive dynamic imports (4)", async () => {
-    const options = await getOptions({root: "test/input/imports", path: "attached.md"});
+    const options = getOptions({root: "test/input/imports", path: "attached.md"});
     const page = parseMarkdown("```js\nimport('./transitive-static-import.js');\n```", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(builtins));
     assert.deepStrictEqual(resolvers.localImports, new Set(["./transitive-static-import.js", "./other/foo.js", "./bar.js"])); // prettier-ignore
   });
   it("detects local dynamic imports", async () => {
-    const options = await getOptions({root: "test/input", path: "attached.md"});
+    const options = getOptions({root: "test/input", path: "attached.md"});
     const page = parseMarkdown("${import('./foo.js')}", options);
     const resolvers = await getResolvers(page, options);
     assert.deepStrictEqual(resolvers.staticImports, new Set(builtins));
