f10a8b467fa7ddfff24cf751f000b2f558912b2b
Philippe Rivière <fil@rezo.net>
2024-04-22 18:08:51 +0200

    add required test (though it's frustrating because the contents will change)

--- test/create-test.ts
+++ test/create-test.ts
@@ -58,6 +58,47 @@ describe("create", () => {
       ])
     );
   });
+  it("instantiates the github template", async () => {
+    const effects = new TestCreateEffects();
+    effects.clack.inputs.push(
+      "./template-github", // Where to create your project?
+      "GitHub Stats", // What to title your project?
+      "github", // Include sample files to help you get started?
+      "", // token
+      "", // repos or org
+      null, // Install dependencies?
+      false // Initialize git repository?
+    );
+    await create(effects);
+    assert.deepStrictEqual(
+      new Set(effects.outputs.keys()),
+      new Set([
+        "template-github/README.md",
+        "template-github/docs/clone.json.ts",
+        "template-github/docs/collaborations.md",
+        "template-github/docs/commits.json.ts",
+        "template-github/docs/components/DOM.js",
+        "template-github/docs/components/color-legend.js",
+        "template-github/docs/components/force-graph.js",
+        "template-github/docs/components/plural.js",
+        "template-github/docs/components/treemap.js",
+        "template-github/docs/config.json.ts",
+        "template-github/docs/config.ts",
+        "template-github/docs/files.json.ts",
+        "template-github/docs/files.md",
+        "template-github/docs/github-repos.ts",
+        "template-github/docs/github.ts",
+        "template-github/docs/index.md",
+        "template-github/docs/issues.json.ts",
+        "template-github/docs/issues.md",
+        "template-github/docs/repos.json.ts",
+        "template-github/docs/repos.md",
+        "template-github/docs/user.json.ts",
+        "template-github/docs/user.md",
+        "template-github/package.json"
+      ])
+    );
+  });
 });
 
 class TestCreateEffects implements CreateEffects {
