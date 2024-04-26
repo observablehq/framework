1bab6fdfe9d01857c8912246a6fc5ae1f2fb1d8e
Philippe Rivière <fil@rezo.net>
2024-04-26 10:55:16 -0700

    simpler focus

--- src/client/search-init.ts
+++ src/client/search-init.ts
@@ -10,22 +10,20 @@ input.addEventListener("focus", load, {once: true});
 input.addEventListener("keydown", load, {once: true});
 
 // Focus on meta-K and /
-const toggle = document.querySelector("#observablehq-sidebar-toggle")!;
+const sidebar = document.querySelector("#observablehq-sidebar")!;
 addEventListener("keydown", (event) => {
   if (
     (event.code === "KeyK" && event.metaKey && !event.altKey && !event.ctrlKey) ||
     (event.key === "/" && !event.metaKey && !event.altKey && !event.ctrlKey && event.target === document.body)
   ) {
     // Force the sidebar to be temporarily open while the search input is
-    // focused. (We can’t use :focus-within because the sidebar isn’t focusable
-    // while it is invisible, and we don’t want to keep the sidebar open
-    // persistently after you blur the search input.)
-    toggle.classList.add("observablehq-sidebar-open");
+    // focused. The class makes the sidebar visible so the input can be focused,
+    // then the :focus-within selector takes over and keeps it open until the
+    // user blurs the input.
+    sidebar.classList.add("observablehq-sidebar-open");
     input.focus();
+    sidebar.classList.remove("observablehq-sidebar-open");
     input.select();
     event.preventDefault();
   }
 });
-
-// Allow the sidebar to close when the search input is blurred.
-input.addEventListener("blur", () => toggle.classList.remove("observablehq-sidebar-open"));
