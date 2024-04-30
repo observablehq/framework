f0755d3b4e958a00190612018ad2dfb38688a39c
Mike Bostock <mbostock@gmail.com>
2024-04-29 18:22:50 -0700

    collapse only child

--- test/tree-test.ts
+++ test/tree-test.ts
@@ -6,49 +6,55 @@ function stree(items: string[]): string[] {
 }
 
 describe("tree(items, options)", () => {
+  it("collapses single-child nodes", () => {
+    assert.deepStrictEqual(stree(["/a/b/c/d"]), [
+      "┌ (1 page)  ",
+      "└── a/b/c/d "
+    ]); // prettier-ignore
+  });
+  it("collapses single-child nodes", () => {
+    assert.deepStrictEqual(stree(["/a/b/c/d", "/a/b/c/e"]), [
+      "┌ (2 pages)         ",
+      "└── a/b/c (2 pages) ",
+      "    ├── d           ",
+      "    └── e           "
+    ]);
+  });
   it("creates implicit internal nodes", () => {
     assert.deepStrictEqual(stree(["/a/b/c/d"]), [
-      "┌ (1 page)             ",
-      "└── a (1 page)         ",
-      "    └── b (1 page)     ",
-      "        └── c (1 page) ",
-      "            └── d      "
-    ]);
+      "┌ (1 page)  ",
+      "└── a/b/c/d "
+    ]); // prettier-ignore
   });
   it("removes the .md extension, if present", () => {
     assert.deepStrictEqual(stree(["/a/b/c/a.md", "/a/b/c/b.md", "/a/b/c/c.md", "/a/b/c/d.md"]), [
-      "┌ (4 pages)             ",
-      "└── a (4 pages)         ",
-      "    └── b (4 pages)     ",
-      "        └── c (4 pages) ",
-      "            ├── a       ",
-      "            ├── b       ",
-      "            ├── c       ",
-      "            └── d       "
+      "┌ (4 pages)         ",
+      "└── a/b/c (4 pages) ",
+      "    ├── a           ",
+      "    ├── b           ",
+      "    ├── c           ",
+      "    └── d           "
     ]);
   });
   it("handles multiple leaves", () => {
     assert.deepStrictEqual(stree(["/a/b/c/a", "/a/b/c/b", "/a/b/c/c", "/a/b/c/d"]), [
-      "┌ (4 pages)             ",
-      "└── a (4 pages)         ",
-      "    └── b (4 pages)     ",
-      "        └── c (4 pages) ",
-      "            ├── a       ",
-      "            ├── b       ",
-      "            ├── c       ",
-      "            └── d       "
+      "┌ (4 pages)         ",
+      "└── a/b/c (4 pages) ",
+      "    ├── a           ",
+      "    ├── b           ",
+      "    ├── c           ",
+      "    └── d           "
     ]);
   });
   it("handles leaves at different levels", () => {
     assert.deepStrictEqual(stree(["/a", "/a/b", "/a/b/c", "/a/b/c/d"]), [
-      "┌ (4 pages)            ",
-      "├── a (3 pages)        ",
-      "│   ├── b (2 pages)    ",
-      "│   │   ├── c (1 page) ",
-      "│   │   │   └── d      ",
-      "│   │   └── c          ",
-      "│   └── b              ",
-      "└── a                  "
+      "┌ (4 pages)         ",
+      "├── a (3 pages)     ",
+      "│   ├── b (2 pages) ",
+      "│   │   ├── c/d     ",
+      "│   │   └── c       ",
+      "│   └── b           ",
+      "└── a               "
     ]);
   });
   it("sorts nodes", () => {
@@ -62,12 +68,11 @@ describe("tree(items, options)", () => {
   });
   it("sorts folders before files", () => {
     assert.deepStrictEqual(stree(["/d/a", "/b", "/c", "/a"]), [
-      "┌ (4 pages)    ",
-      "├── d (1 page) ",
-      "│   └── a      ",
-      "├── a          ",
-      "├── b          ",
-      "└── c          "
+      "┌ (4 pages) ",
+      "├── d/a     ",
+      "├── a       ",
+      "├── b       ",
+      "└── c       "
     ]);
   });
   it("sorts nested folders", () => {
