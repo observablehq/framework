c53932fd4a4e2bd3734a756455c2c1c0b1989e7d
Mike Bostock <mbostock@gmail.com>
2024-05-08 11:49:49 -0700

    simplify snowflake query

--- examples/loader-snowflake/src/data/api-requests.csv.ts
+++ examples/loader-snowflake/src/data/api-requests.csv.ts
@@ -1,18 +1,19 @@
 import {csvFormat} from "d3-dsv";
-import {utcDay} from "d3-time";
 import {run} from "./snowflake.js";
 
-const end = utcDay();
-const start = utcDay.offset(end, -1);
+const start = new Date("2024-01-01");
+const end = new Date("2024-01-08");
 
 process.stdout.write(
   csvFormat(
     await run((query) =>
       query(
-        `SELECT normalized_path AS "path", COUNT(*) AS "count"
+        `SELECT '/' || path AS "path", COUNT(*) AS "count"
 FROM fct_api_logs
 WHERE time between :1 and :2
+AND STARTSWITH(path, 'document/@d3/')
 GROUP BY 1
+HAVING "count" >= 10
 ORDER BY 2 DESC`,
         [start, end]
       )
