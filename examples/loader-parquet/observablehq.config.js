80a4d457ea685ff820b8a082445ffb9e36ede05e
Mike Bostock <mbostock@gmail.com>
2024-05-09 15:48:51 -0700

    fix source link

--- examples/loader-parquet/observablehq.config.js
+++ examples/loader-parquet/observablehq.config.js
@@ -54,7 +54,7 @@ export default {
       <span class="hide-if-small">Observable</span> Framework
     </a>
     <span style="display: flex; align-items: baseline; gap: 0.5rem; font-size: 14px;">
-      <a target="_blank" href="https://github.com/observablehq/framework/tree/main/examples/loader-arrow"><span>View source</span></a>
+      <a target="_blank" href="https://github.com/observablehq/framework/tree/main/examples/loader-parquet"><span>View source</span></a>
     </span>
   </div>
 </div>`
