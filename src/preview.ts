e6af5349ed94e6e5670aec99b783d33aeec521f8
Mike Bostock <mbostock@gmail.com>
2024-06-05 08:43:53 -0700

    no wrapper span (#1416)
    * no wrapper span
    
    * fix top-level text
    
    * simplify inline syntax error
    
    * comment marker
    
    * fix table loading
    
    * fix iterable of non-node
    
    * robust placeholder parser
    
    * replacement strategy
    
    * fix backslashes
    
    * drop invalid interpolations
    
    * comments
    
    * simplify
    
    * remove unused codeErrors
    
    * preserve comments when highlighting
    
    * handle fenced code blocks
    
    * more better parser
    
    * clean and comment
    
    * cleaner
    
    * fix fences within comments
    
    * Update test/placeholder-test.ts
    
    Co-authored-by: Philippe Rivière <fil@rezo.net>
    
    * ignore invalid tokens
    
    * root.parentNode for visibility
    
    ---------
    
    Co-authored-by: Philippe Rivière <fil@rezo.net>

--- src/preview.ts
+++ src/preview.ts
@@ -8,6 +8,7 @@ import {basename, dirname, join, normalize} from "node:path/posix";
 import {difference} from "d3-array";
 import type {PatchItem} from "fast-array-diff";
 import {getPatch} from "fast-array-diff";
+import deepEqual from "fast-deep-equal";
 import mime from "mime";
 import openBrowser from "open";
 import send from "send";
@@ -19,7 +20,7 @@ import type {LoaderResolver} from "./dataloader.js";
 import {HttpError, isEnoent, isHttpError, isSystemError} from "./error.js";
 import {getClientPath} from "./files.js";
 import type {FileWatchers} from "./fileWatchers.js";
-import {parseHtml, rewriteHtml} from "./html.js";
+import {isComment, isElement, isText, parseHtml, rewriteHtml} from "./html.js";
 import {transpileJavaScript, transpileModule} from "./javascript/transpile.js";
 import {parseMarkdown} from "./markdown.js";
 import type {MarkdownCode, MarkdownPage} from "./markdown.js";
@@ -285,11 +286,16 @@ function getWatchFiles(resolvers: Resolvers): Iterable<string> {
   return files;
 }
 
+interface HtmlPart {
+  type: number;
+  value: string;
+}
+
 function handleWatch(socket: WebSocket, req: IncomingMessage, configPromise: Promise<Config>) {
   let config: Config | null = null;
   let path: string | null = null;
   let hash: string | null = null;
-  let html: string[] | null = null;
+  let html: HtmlPart[] | null = null;
   let code: Map<string, string> | null = null;
   let files: Map<string, string> | null = null;
   let tables: Map<string, string> | null = null;
@@ -429,8 +435,19 @@ function handleWatch(socket: WebSocket, req: IncomingMessage, configPromise: Pro
   }
 }
 
-function getHtml({body}: MarkdownPage, resolvers: Resolvers): string[] {
-  return Array.from(parseHtml(rewriteHtml(body, resolvers)).document.body.children, (d) => d.outerHTML);
+function serializeHtml(node: ChildNode): HtmlPart | undefined {
+  return isElement(node)
+    ? {type: 1, value: node.outerHTML}
+    : isText(node)
+    ? {type: 3, value: node.nodeValue!}
+    : isComment(node)
+    ? {type: 8, value: node.data}
+    : undefined;
+}
+
+function getHtml({body}: MarkdownPage, resolvers: Resolvers): HtmlPart[] {
+  const {document} = parseHtml(`\n${rewriteHtml(body, resolvers)}`);
+  return Array.from(document.body.childNodes, serializeHtml).filter((d): d is HtmlPart => d != null);
 }
 
 function getCode({code}: MarkdownPage, resolvers: Resolvers): Map<string, string> {
@@ -530,8 +547,8 @@ function diffTables(
   return patch;
 }
 
-function diffHtml(oldHtml: string[], newHtml: string[]): RedactedPatch<string> {
-  return getPatch(oldHtml, newHtml).map(redactPatch);
+function diffHtml(oldHtml: HtmlPart[], newHtml: HtmlPart[]): RedactedPatch<HtmlPart> {
+  return getPatch(oldHtml, newHtml, deepEqual).map(redactPatch);
 }
 
 type RedactedPatch<T> = RedactedPatchItem<T>[];
---
+++ test/placeholder-test.ts
@@ -0,0 +1,265 @@
+import assert from "node:assert";
+import {parsePlaceholder} from "../src/placeholder.js";
+
+function placeholders(input: string, start = 0): {type: string; value: string}[] {
+  return Array.from(parsePlaceholder(input, start));
+}
+
+describe("parsePlaceholder", () => {
+  it("finds placeholders within data", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "'world'"},
+        {type: "content", value: "!"}
+      ],
+      placeholders("Hello, ${'world'}!")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "'world'"}
+      ],
+      placeholders("Hello, ${'world'}")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "code", value: "'world'"},
+        {type: "content", value: "!"}
+      ],
+      placeholders("${'world'}!")
+    );
+    assert.deepStrictEqual([{type: "code", value: "'world'"}], placeholders("${'world'}"));
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, _"},
+        {type: "code", value: "'world'"},
+        {type: "content", value: "_"}
+      ],
+      placeholders("Hello, _${'world'}_")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "1<"},
+        {type: "code", value: "2"}
+      ],
+      placeholders("1<${2}")
+    );
+  });
+  it("finds multiple placeholders", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "("},
+        {type: "code", value: "1"},
+        {type: "content", value: " "},
+        {type: "code", value: "2"},
+        {type: "content", value: " "},
+        {type: "code", value: "3"},
+        {type: "content", value: " "},
+        {type: "code", value: "4"},
+        {type: "content", value: ")"}
+      ],
+      placeholders("(${1} ${2} ${3} ${4})")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "("},
+        {type: "code", value: "1"},
+        {type: "code", value: "2"},
+        {type: "content", value: "+"},
+        {type: "code", value: "3"},
+        {type: "content", value: " + "},
+        {type: "code", value: "4"},
+        {type: "content", value: ")"}
+      ],
+      placeholders("(${1}${2}+${3} + ${4})")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "code", value: "1"},
+        {type: "code", value: "2"},
+        {type: "code", value: "3"},
+        {type: "code", value: "4"}
+      ],
+      placeholders("${1}${2}${3}${4}")
+    );
+  });
+  it("finds empty placeholders", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "("},
+        {type: "code", value: ""},
+        {type: "content", value: ")"}
+      ],
+      placeholders("(${})")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "code", value: ""},
+        {type: "code", value: ""}
+      ],
+      placeholders("${}${}")
+    );
+  });
+  it("removes backslash before dollar sign", () => {
+    assert.deepStrictEqual([{type: "content", value: "(${})"}], placeholders("(\\${})"));
+    assert.deepStrictEqual(
+      [{type: "content", value: "($)"}],
+      placeholders("(\\$)") // curly braces not required
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "(\\${})"}],
+      placeholders("(\\\\${})") // only the last backslash is special
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "(\\\\${})"}],
+      placeholders("(\\\\\\${})") // only the last backslash is special
+    );
+  });
+  it("removes backslashes after dollar signs and before left braces", () => {
+    assert.deepStrictEqual([{type: "content", value: "(${})"}], placeholders("($\\{})"));
+    assert.deepStrictEqual(
+      [{type: "content", value: "(${)"}],
+      placeholders("($\\{)") // curly braces not required
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "(\\{})"}],
+      placeholders("(\\{})") // dollar sign is required
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "($\\{})"}],
+      placeholders("(\\$\\{})") // dollar sign must not be escaped
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "($\\{})"}],
+      placeholders("($\\\\{})") // only the last backslash is special
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "($\\\\{})"}],
+      placeholders("($\\\\\\{})") // only the last backslash is special
+    );
+  });
+  it("does not remove backslashes before other left braces", () => {
+    assert.deepStrictEqual([{type: "content", value: "(\\{})"}], placeholders("(\\{})"));
+  });
+  it("parses comments within placeholders", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "1 /*}*/ + 2"}
+      ],
+      placeholders("Hello, ${1 /*}*/ + 2}")
+    );
+  });
+  it("parses braces within placeholders", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "{foo: {bar: {}}}"}
+      ],
+      placeholders("Hello, ${{foo: {bar: {}}}}")
+    );
+    assert.deepStrictEqual(
+      [{type: "code", value: "(function*() { yield 42; })()"}],
+      placeholders("${(function*() { yield 42; })()}")
+    );
+    assert.deepStrictEqual(
+      [{type: "code", value: "(async function*() { await (yield 42); })()"}],
+      placeholders("${(async function*() { await (yield 42); })()}")
+    );
+  });
+  it("parses invalid syntax", () => {
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "1,"}
+      ],
+      placeholders("Hello, ${1,}")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "void"}
+      ],
+      placeholders("Hello, ${void}")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "foo: [1, 2]"}
+      ],
+      placeholders("Hello, ${foo: [1, 2]}")
+    );
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "Hello, "},
+        {type: "code", value: "yield 42"}
+      ],
+      placeholders("Hello, ${yield 42}")
+    );
+  });
+  it("ignores placeholders with invalid tokens", () => {
+    assert.deepStrictEqual([{type: "content", value: "Hello, ${`1,}!"}], placeholders("Hello, ${`1,}!"));
+    assert.deepStrictEqual([{type: "content", value: "Hello, ${'\\u'}!"}], placeholders("Hello, ${'\\u'}!"));
+  });
+  // Note: this case is not encountered in practice due to markdown-it tokenization.
+  it("ignores placeholders within attributes", () => {
+    assert.deepStrictEqual(
+      [{type: "content", value: "<a href=${'link'}>text</a>"}],
+      placeholders("<a href=${'link'}>text</a>")
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "<a href='${'link'}'>text</a>"}],
+      placeholders("<a href='${'link'}'>text</a>")
+    );
+  });
+  // Note: this case is not encountered in practice due to markdown-it tokenization.
+  it("ignores placeholders within tags", () => {
+    assert.deepStrictEqual([{type: "content", value: "<a ${'attr'}>text</a>"}], placeholders("<a ${'attr'}>text</a>"));
+  });
+  it("ignores placeholders within rawtext", () => {
+    assert.deepStrictEqual(
+      [{type: "content", value: "<script>${1 + 2}</script>"}],
+      placeholders("<script>${1 + 2}</script>")
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "<style>${1 + 2}</style>"}],
+      placeholders("<style>${1 + 2}</style>")
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "<textarea>${1 + 2}</textarea>"}],
+      placeholders("<textarea>${1 + 2}</textarea>")
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "<title>${1 + 2}</title>"}],
+      placeholders("<title>${1 + 2}</title>")
+    );
+  });
+  it("ignores placeholders within code", () => {
+    assert.deepStrictEqual([{type: "content", value: "`${1 + 2}`"}], placeholders("`${1 + 2}`"));
+    assert.deepStrictEqual([{type: "content", value: "``${1 + 2}``"}], placeholders("``${1 + 2}``"));
+    assert.deepStrictEqual([{type: "content", value: "``${`1 + 2`}``"}], placeholders("``${`1 + 2`}``"));
+  });
+  it("ignores placeholders within comment", () => {
+    assert.deepStrictEqual([{type: "content", value: "<!--${1 + 2}-->"}], placeholders("<!--${1 + 2}-->"));
+    assert.deepStrictEqual([{type: "content", value: "<!-- ${1 + 2} -->"}], placeholders("<!-- ${1 + 2} -->"));
+    assert.deepStrictEqual(
+      [
+        {type: "content", value: "<!--```\n\n${1}\n\n```-->\n"},
+        {type: "code", value: "1"}
+      ],
+      placeholders("<!--```\n\n${1}\n\n```-->\n${1}")
+    );
+  });
+  it("ignores unterminated placeholders", () => {
+    assert.deepStrictEqual([{type: "content", value: "${1 + 2"}], placeholders("${1 + 2"));
+    assert.deepStrictEqual(
+      [{type: "content", value: "Hello, ${{foo: [1, 2]}"}],
+      placeholders("Hello, ${{foo: [1, 2]}")
+    );
+    assert.deepStrictEqual(
+      [{type: "content", value: "Hello, ${`unterminated}"}],
+      placeholders("Hello, ${`unterminated}")
+    );
+  });
+});
