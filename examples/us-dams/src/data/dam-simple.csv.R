9dabdc91cc81e02a3c80dae08868724c5a5c9cdd
Allison Horst <allison.horst@gmail.com>
2024-06-14 11:47:42 -0700

    clean up unused columns in loader

--- examples/us-dams/src/data/dam-simple.csv.R
+++ examples/us-dams/src/data/dam-simple.csv.R
@@ -11,18 +11,11 @@ dams <- fread("https://nid.sec.usace.army.mil/api/nation/csv") |>
                 state = State,
                 county = County,
                 city = City,
-                # waterway = 'River or Stream Name',
                 primaryDamType = 'Primary Dam Type',
-                # heightFt = 'Dam Height (Ft)',
-                # length = 'Dam Length (Ft)',
                 yearCompleted = 'Year Completed',
-                # yearsModified = 'Years Modified',
                 maxStorageAcreFt = 'Max Storage (Acre-Ft)',
-                # lastInspectionDate = 'Last Inspection Date',
-                # inspectionFrequency = 'Inspection Frequency',
                 hazardPotential = 'Hazard Potential Classification',
                 conditionAssessment = 'Condition Assessment',
-                # conditionAssessmentDate = 'Condition Assessment Date'
                 ) |>
   dplyr::mutate(conditionAssessment = case_when(
     conditionAssessment %in% c(NA, "Not Available", "Not Rated") ~ "Not available",
