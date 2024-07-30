ee492330d253360f4b5c5e071f457da116da67a5
Philippe Rivière <fil@rezo.net>
2024-07-30 19:41:41 +0200

    expand the inspector if its previous version was expanded (#1475)
    * expand the inspector if its previous version was expanded
    this should be revised more thoroughly when we fix https://github.com/observablehq/inspector/issues/12
    
    closes #1458
    
    * fix multiple display; apply suggestions from code review
    
    * fix comment
    
    * preserve deep expanded state
    
    * handle missing expanded state
    
    ---------
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>

--- src/client/inspect.js
+++ src/client/inspect.js
@@ -1,8 +1,15 @@
 import {Inspector} from "observablehq:runtime";
 
-export function inspect(value) {
+export function inspect(value, expanded) {
   const node = document.createElement("div");
   new Inspector(node).fulfilled(value);
+  if (expanded) {
+    for (const path of expanded) {
+      let child = node;
+      for (const i of path) child = child?.childNodes[i];
+      child?.dispatchEvent(new Event("mouseup")); // restore expanded state
+    }
+  }
   return node;
 }
 
