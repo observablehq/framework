4e4a4e934b759f6bd6b92be4ada0867f8f2db07e
Philippe Rivière <fil@rezo.net>
2024-08-20 17:26:43 +0200

    move the links to Inter+Spline into the head don't duplicate the preconnect link

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -86,7 +86,9 @@ export default {
     {name: "Contributing", path: "/contributing", pager: false}
   ],
   base: "/framework",
-  head: `<link rel="apple-touch-icon" href="/observable.png">
+  head: `<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Spline+Sans+Mono:ital,wght@0,300..700;1,300..700&display=swap" crossorigin>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Spline+Sans+Mono:ital,wght@0,300..700;1,300..700&display=swap" crossorigin>
+<link rel="apple-touch-icon" href="/observable.png">
 <link rel="icon" type="image/png" href="/observable.png" sizes="32x32">${
     process.env.CI
       ? `
