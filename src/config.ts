1980ecaea552e8c2ae3f33c5e9c5fe3b21e697bf
Mike Bostock <mbostock@gmail.com>
2024-08-21 21:09:28 -0600

    Source Serif 4

--- src/config.ts
+++ src/config.ts
@@ -291,7 +291,7 @@ function pageFragment(spec: unknown): PageFragmentFunction | string | null {
 
 function defaultDefaultStylesheets(): string[] {
   return [
-    "https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+    "https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
   ];
 }
 
