32b0ba870ad296dabcec3fa40c83c8c6f567091a
Mike Bostock <mbostock@gmail.com>
2024-08-21 21:12:12 -0600

    fix test snapshots

--- test/config-test.ts
+++ test/config-test.ts
@@ -17,7 +17,7 @@ describe("readConfig(undefined, root)", () => {
       base: "/",
       style: {theme: ["air", "near-midnight"]},
       defaultStylesheets: [
-        "https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+        "https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
       ],
       sidebar: true,
       pages: [
@@ -56,7 +56,7 @@ describe("readConfig(undefined, root)", () => {
       base: "/",
       style: {theme: ["air", "near-midnight"]},
       defaultStylesheets: [
-        "https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+        "https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
       ],
       sidebar: true,
       pages: [{name: "Build test case", path: "/simple", pager: "main"}],
