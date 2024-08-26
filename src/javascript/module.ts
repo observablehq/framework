bdc332c435626df7f8a0ca5cc1423d808f38b9f4
Mike Bostock <mbostock@gmail.com>
2024-08-26 10:25:23 -0700

    error if empty extension

--- src/javascript/module.ts
+++ src/javascript/module.ts
@@ -209,7 +209,8 @@ export function getFileInfo(root: string, path: string): FileInfo | undefined {
 }
 
 export function findModule(root: string, path: string): RouteResult | undefined {
-  const ext = extname(path) || ".js";
+  const ext = extname(path);
+  if (!ext) throw new Error(`empty extension: ${path}`);
   const exts = [ext];
   if (ext === ".js") exts.push(".jsx");
   return route(root, path.slice(0, -ext.length), exts);
