eb4bb9015e7c4901c746426b89de671bcbcc748c
Mike Bostock <mbostock@gmail.com>
2024-08-29 12:59:51 -0700

    move docs around

--- docs/page-loaders.md.js
+++ docs/page-loaders.md.js
@@ -4,11 +4,11 @@ keywords: server-side rendering, ssr
 
 # Page loaders <a href="https://github.com/observablehq/framework/pull/1523" class="observablehq-version-badge" data-version="prerelease" title="Added in #1523"></a>
 
-Page loaders are like [data loaders](./data-loaders), but for dynamically generating (or “server-side rendering”) pages. Page loaders are programs that emit [Markdown](./markdown) to standard out, and have a double extension starting with \`.md\`, such as \`.md.js\` for a JavaScript page loader or \`.md.py\` for a Python page loader.
+Page loaders are a special type of [data loader](./data-loaders) for dynamically generating (or “server-side rendering”) pages. Page loaders are programs that emit [Markdown](./markdown) to standard out, and have a double extension starting with \`.md\`, such as \`.md.js\` for a JavaScript page loader or \`.md.py\` for a Python page loader.
 
 By “baking” dynamically-generated content into static Markdown, you can further improve the performance of pages since the content exists on page load rather than waiting for JavaScript to run. You may even be able to avoid loading additional assets and JavaScript libraries.
 
-For example, to render a map of recent earthquakes into static SVG using D3:
+For example, to render a map of recent earthquakes into static inline SVG using D3, you could use a JavaScript page loader as \`quakes.md.js\`:
 
 ~~~js run=false
 import * as d3 from "d3-geo";
@@ -32,32 +32,19 @@ process.stdout.write(\`# Recent quakes
 \`);
 ~~~
 
+See the [data loaders](./data-loaders) documentation for more on execution, routing, and caching.
+
+<div class="tip">
+
+Page loaders often use [parameterized routes](./params) to generate multiple pages from a single program.
+
+</div>
+
 <div class="tip">
 
 When using page loaders, keep an eye on the generated page size, particularly with complex maps and data visualizations in SVG. To keep the page size small, consider server-side rendering a low-fidelity placeholder and then replacing it with the full graphic using JavaScript on the client.
 
 </div>
-
-## Parameterized pages
-
-Page loaders often use [parameterized routes](./params) to generate multiple pages from a single program. Parameter values are passed as command-line arguments. In a JavaScript page loader, you can use [\`parseArgs\`](https://nodejs.org/api/util.html#utilparseargsconfig) from \`node:util\` to parse command-line arguments. You can then bake parameter values into the resulting page, or reference them dynamically [in client-side JavaScript](./params#params-in-JavaScript) using \`observable.params\`.
-
-~~~~js run=false
-import {parseArgs} from "node:util";
-
-const {
-  values: {product}
-} = parseArgs({
-  options: {product: {type: "string"}}
-});
-
-process.stdout.write(\`# Hello $\{product}
-
-Or alternatively, $\\{observable.params.product}.
-\`);
-~~~~
-
-Framework’s [theme previews](./themes) are implemented as parameterized page loaders; see [their source](https://github.com/observablehq/framework/blob/main/docs/theme/%5Btheme%5D.md.ts) for a practical example.
 
 <div class="tip">
 
