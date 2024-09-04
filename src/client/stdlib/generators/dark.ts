343a1bf395b2d53d6414659eb6242ee311fe4738
Philippe Rivière <fil@rezo.net>
2024-09-04 09:44:46 +0200

    not necessary

--- src/client/stdlib/generators/dark.ts
+++ src/client/stdlib/generators/dark.ts
@@ -6,7 +6,6 @@ export function dark(target: HTMLElement = document.body) {
     let dark: boolean | undefined;
     target.style.setProperty("transition-property", "color");
     target.style.setProperty("transition-duration", "0.001s");
-    target.style.setProperty("transition-behavior", "allow-discrete"); // TODO: not sure this is necessary
     const changed = () => {
       const d = getComputedStyle(target).getPropertyValue("color-scheme") === "dark";
       if (dark === d) return; // only notify if changed
