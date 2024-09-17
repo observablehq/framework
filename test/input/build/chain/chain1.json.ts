8997f06e63810664cf25560b422ffb5af573f649
Philippe Rivière <fil@rezo.net>
2024-09-17 14:45:00 +0200

    fix chain test

--- test/input/build/chain/chain1.json.ts
+++ test/input/build/chain/chain1.json.ts
@@ -1,3 +1,3 @@
 const {FILE_SERVER} = process.env;
 const {x} = await fetch(`${FILE_SERVER}chain-source.json`).then((response) => response.json());
-console.log(JSON.stringify({x, "x^2": x * x}, null, 2));
+process.stdout.write(JSON.stringify({x, "x^2": x * x}, null, 2));
--- test/input/build/chain/chain2.csv.ts
+++ test/input/build/chain/chain2.csv.ts
@@ -1,3 +1,3 @@
 const {FILE_SERVER} = process.env;
 const {x} = await fetch(`${FILE_SERVER}chain-source.json`).then((response) => response.json());
-console.log(`name,value\nx,${x}\nx^2,${x * x}`);
+process.stdout.write(`name,value\nx,${x}\nx^2,${x * x}`);
