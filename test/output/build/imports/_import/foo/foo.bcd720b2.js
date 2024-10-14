5ab16ab33d7cbba5f7fe6dbfd8bc11121594ff03
Mike Bostock <mbostock@gmail.com>
2024-10-14 14:46:07 -0400

    test

--- test/output/build/imports/_import/foo/foo.bcd720b2.js
+++ test/output/build/imports/_import/foo/foo.bcd720b2.js
@@ -1,5 +1,6 @@
 import "../../_npm/d3@7.8.5/cd372fb8.js";
-import {bar} from "../bar/bar.13bb8056.js";
+import "../../_npm/@example/url-import@1.0.0/1dd108c5.js";
+import {bar} from "../bar/bar.4460ccc2.js";
 export {top} from "../top.160847a6.js";
 
 export const foo = "foo";
