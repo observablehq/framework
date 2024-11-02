a23c35c1935c4891e7a0edfc55742026d0dfccf9
--global <--global>
2024-11-01 23:12:32 -0700

    set initial branch when initializing repo instead of setting the default branch after repo is already made, which of course does nothing for the current repo

--- test/mocks/directory.ts
+++ test/mocks/directory.ts
@@ -13,7 +13,7 @@ export function mockIsolatedDirectory({git}: {git: boolean}) {
     process.chdir(dir);
     if (git) {
       await promisify(exec)(
-        'git init && git config user.email "observable@example.com" && git config user.name "Observable User" && git config init.defaultBranch main'
+        'git init -b main && git config user.email "observable@example.com" && git config user.name "Observable User"'
       );
     }
   });
