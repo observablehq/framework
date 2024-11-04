0de572fa4b4d925fbbe215d88ea8dda073b30930
Philippe Rivière <fil@rezo.net>
2024-11-04 12:31:34 +0100

    prettier

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -14,7 +14,15 @@ import type {LoaderResolver} from "./loader.js";
 import type {MarkdownPage} from "./markdown.js";
 import {extractNodeSpecifier, resolveNodeImport, resolveNodeImports} from "./node.js";
 import {extractNpmSpecifier, populateNpmCache, resolveNpmImport, resolveNpmImports} from "./npm.js";
-import {isAssetPath, isPathImport, parseRelativeUrl, relativePath, resolveLocalPath, resolvePath, resolveRelativePath} from "./path.js";
+import {
+  isAssetPath,
+  isPathImport,
+  parseRelativeUrl,
+  relativePath,
+  resolveLocalPath,
+  resolvePath,
+  resolveRelativePath
+} from "./path.js";
 
 export interface Resolvers {
   path: string;
